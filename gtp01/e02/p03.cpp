10197653d41cc3449083aa9956a95ef40cca8a4b
esteban <esteban@ecfornal@yahoo.com.ar>
2010-03-20 12:47:52 -0300

    agregados parametros para display de una cimglist

--- gtp01/e02/p03.cpp
+++ gtp01/e02/p03.cpp
@@ -1,3 +1,14 @@
+/*
+  # lista.display(disp2,'x','p');
+  \param axis  : The axis used to append the image for visualization. 
+    Can be 'x' (default),'y','z' or 'c'.
+  \param align : Defines the relative alignment of images when displaying 
+    images of different sizes.
+    Can be '\p c' (centered, which is the default), 
+    '\p p' (top alignment) and 
+    '\p n' (bottom aligment).
+*/
+
 #include <CImg.h>
 #include <iostream>
 
@@ -13,9 +24,29 @@ int main() {
 
     lista.print();
 
-    CImgDisplay disp;
-    lista.display(disp);
-    while( !disp.is_closed() && !disp.is_keyQ() ){} 
-    
+    CImgDisplay disp1;
+    lista.display(disp1,'x','p');
+    while( !disp1.is_closed() && !disp1.is_keyQ() ){} 
+
+    CImgDisplay disp2;
+    lista.display(disp2,'y','p');
+    while( !disp2.is_closed() && !disp2.is_keyQ() ){} 
+
+    CImgDisplay disp3;
+    lista.display(disp3,'z','p');
+    while( !disp3.is_closed() && !disp3.is_keyQ() ){} 
+
+    CImgDisplay disp4;
+    lista.display(disp4,'c','p');
+    while( !disp4.is_closed() && !disp4.is_keyQ() ){} 
+
+    CImgDisplay disp5;
+    lista.display(disp5,'x','n');
+    while( !disp5.is_closed() && !disp5.is_keyQ() ){} 
+
+    CImgDisplay disp6;
+    lista.display(disp6,'x','c');
+    while( !disp6.is_closed() && !disp6.is_keyQ() ){} 
+
     return 0;
 }
