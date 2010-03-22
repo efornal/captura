e8dcbdeb6a85119bc12f3ea3bc66097a8375189d
christian <c.pfarher@gmail.com>
2010-03-22 17:52:08 -0300

    agrego ejer 3 guia 2

--- christian_workspace/tp2/tp2_fuente/tp2_ejer2.cpp
+++ christian_workspace/tp2/tp2_fuente/tp2_ejer2.cpp
@@ -15,15 +15,17 @@ int main(int argc, char *argv[]) {
 	const char *filename = "/home/christian/Documentos/universidad/2010/captura/git_repo/captura/christian_workspace/tp1/img.jpg";
 CImg <float> imagen;
 imagen.load(filename);
-imagen.display();
+imagen.display(); //tira info de lox pixels y demas
+
+CImgDisplay ventana(imagen, "imagen"); //conserva el tamanio y no tira informacion ninguna
 
 //CImg <char> imagen(filename);
 //CImgDisplay ventana(imagen, "earth");
 
 /*CImgDisplay vent1(imagen, "imagen 0"), vent2(imagen1, "imagen 1"), vent3(
- imagen2, "imagen 2");
- while (!vent1.is_closed()) {
- }*/
+ imagen2, "imagen 2");*/
+ while (!ventana.is_closed()) {
+ }
 
 return 0;
 }
