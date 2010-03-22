e640a95ec931eb9bb05c8f3875908d248ac86b48
esteban <esteban@ecfornal@yahoo.com.ar>
2010-03-21 21:50:11 -0300

    acomodados punto uno y dos que estaban intercambiados

--- gtp01/e05/p01.cpp
+++ gtp01/e05/p01.cpp
@@ -1,7 +1,3 @@
-/**
-   FIXME:
-     1. Esta bien hecho el submuestreo con resize como abajo?
- */
 #include <CImg.h>
 #include <iostream>
 
@@ -9,16 +5,9 @@ using namespace cimg_library;
 using namespace std;
 
 int main( int argc, char **argv ) {
-    const int factor = cimg_option( "-f", 2, "factor de submuestreo" );
-
     CImg<unsigned char> img1( "../../imagenes/rmn.jpg" );
 
     CImgDisplay disp ( img1, "imagen rmn.jpg", 1 );
-
-    img1.print();
-    
-    img1.get_resize( (int)(img1.width()/factor), (int)(img1.height()/factor) ).
-        display();
 
     while ( !disp.is_closed() && !disp.is_keyQ() ) {}
     return 0;
