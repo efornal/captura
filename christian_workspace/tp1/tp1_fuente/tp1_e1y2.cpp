f3c8e2ca272b0fe1630f65ed2a6b3999b65249c9
christian <c.pfarher@gmail.com>
2010-03-22 16:18:32 -0300

    agrego tp1 y 2

---
+++ christian_workspace/tp1/tp1_fuente/tp1_e1y2.cpp
@@ -0,0 +1,42 @@
+//============================================================================
+// Name        : tutorial.cpp
+// Author      : Christian Pfarher
+// Version     :
+// Copyright   : GNU Public License
+// Description : Captura 2010, Ansi-style
+//============================================================================
+
+#include <CImg.h>
+#include <iostream>
+using namespace cimg_library;
+using namespace std;
+int main(int argc, char *argv[]) {
+	CImg <float> original_image("/home/christian/Documentos/universidad/2010/captura/imagenes/chairs.jpg"); //cargo imagen
+	original_image.display("canal por defecto");
+
+
+
+	CImg<float> image_ch0 (original_image);
+	CImg<float> image_ch1 (original_image);
+	CImg<float> image_ch2 (original_image);
+	CImg<float> image_ch3 (original_image);
+
+
+	image_ch0.channel(0);
+	image_ch0.display("canal seteado en 0");
+
+	image_ch1.channel(1);
+	image_ch1.display("canal seteado en 1");
+
+	image_ch2.channel(2);
+	image_ch2.display("canal seteado en 2");
+
+	image_ch3.channel(3);
+	image_ch3.display("canal seteado en 3");
+	//Imagen.load(); y la cargue en el constructor
+
+	CImgDisplay original_withid(original_image, "ventana con CimgDisplay", 0);
+	while (!original_withid.is_closed()) { }
+
+	return 0;
+}
---
+++ christian_workspace/tp1/tp1_fuente/tutorial.cpp
@@ -0,0 +1,42 @@
+//============================================================================
+// Name        : tutorial.cpp
+// Author      : Christina Pfarher
+// Version     :
+// Copyright   : GNU Public License
+// Description : Captura 2010, Ansi-style
+//============================================================================
+
+#include <CImg.h>
+#include <iostream>
+using namespace cimg_library;
+int main(int argc, char *argv[]) {
+	cimg_usage("Ejemplo basico.");
+	const char
+			* filename =
+					cimg_option("-i","/home/christian/Documentos/universidad/2010/captura/imagenes/huang2_corrida.tif","Image file\n");
+	//Imagen color, 8 bits de profundidad por componente.
+	CImg<unsigned char> img(640, 400, 1, 3);
+	img.fill(0); //Asigna 0 a todos los p ́xeles
+	unsigned char rojo[] = { 255, 0, 0 }; //Define color rojo
+	img.draw_text(100, 100, "Hello World", rojo); //Escribe en (100,100).
+	img.display("Hola..."); //Visualiza
+	/*std::cout<<"depth: "<<img._depth<<std::endl;
+	std::cout<<"hight: "<<img._height<<std::endl;
+	std::cout<<"spectrum: "<<img._spectrum<<std::endl;
+	std::cout<<"data: "<<img._data<<std::endl;*/
+	//std::cout<<"data: "<<std::endl;
+
+	//cout<<endl<<"img_depth: "<<img._depth<<endl;
+
+	CImg<unsigned char> img1(filename); //Carga de disco
+	//Copia lo que hay en img1 pero como valores de punto flotante
+	CImg<float> img2(img1);
+	img2.normalize(0, 1); //Normaliza en [0,1]
+	img2.sqrt(); //Aplica ra ́z cuadrada a cada pixel
+	img2.resize(250, 150); //Redimensiona a 250x150
+	//Visualiza ambas im ́genes a la vez
+	CImgDisplay vent1(img1, "original"), vent2(img2, "procesada");
+	while (!vent1.is_closed()) {
+	}
+	return 0;
+}
