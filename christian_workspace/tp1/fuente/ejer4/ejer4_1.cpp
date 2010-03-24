85afcef69a10b754375013153a0b68b0ccf7ffdf
christian <c.pfarher@gmail.com>
2010-03-24 15:38:15 -0300

    normalizado y hecho el ejercicio 4

--- christian_workspace/tp1/fuente/ejer4/ejer4_1.cpp
+++ christian_workspace/tp1/fuente/ejer4/ejer4_1.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : tp2.cpp
+// Name        :
 // Author      : Christian Pfarher
 // Version     :
 // Copyright   : GNU Public License
@@ -12,13 +12,16 @@ using namespace cimg_library;
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	CImg<int> imagen(256, 256, 1, 1);
+	cimg_forXY(imagen, x,y)
+		{
+			imagen(x, y) = y;
+		}
+	int normalizado = 1;
+	CImgDisplay ventana(imagen, "tonos de grises... columnas de 0 a 255",
+			normalizado);//ojo si no lo normalizo no me pone tod en gris
 
-//			"/home/christian/Documentos/universidad/2010/captura/git_repo/captura/christian_workspace/tp1/img.jpg";
-	CImg <unsigned char> imagen;
-	cimg_forXY(imagen, x, y){
+	while (!ventana.is_closed()) {
 	}
-
-
-
 	return 0;
 }
