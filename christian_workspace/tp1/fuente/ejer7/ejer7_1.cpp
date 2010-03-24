986ec7a0e265d106722c110d70ea9f5d2659641a
christian <c.pfarher@gmail.com>
2010-03-24 19:24:22 -0300

    ejercicio 7.1

---
+++ christian_workspace/tp1/fuente/ejer7/ejer7_1.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        :
+// Author      : Christian Pfarher
+// Version     :
+// Copyright   : GNU Public License
+// Description : Captura 2010, Ansi-style
+//============================================================================
+
+#include <CImg.h>
+#include <iostream>
+using namespace cimg_library;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
