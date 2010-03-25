2edbf0fb3bef241b248ae3e9cc1d73c3a50acd77
esteban <esteban@gravis.(none)>
2010-03-25 16:20:38 -0300

    funcion aparte para lut

--- gtp02/e01/p01.cpp
+++ gtp02/e01/p01.cpp
@@ -1,23 +1,26 @@
 #include <CImg.h>
 #include <iostream>
-//include <lut.h>
-
 using namespace cimg_library;
 using namespace std;
 
+CImg<unsigned char> lut( CImg<unsigned char> img1, int a=1, int c=0 ) {
+
+    CImg<unsigned char> img2(img1.width(),img1.height(),1,1);
+    cimg_forXY( img1, x, y ) {
+        img2(x,y) =  a*img1(x,y) + c;
+    }
+    return img2; 
+}
+
 int main() {
 
-    int a = 2, c = 0;
-
     CImg<unsigned char> img1("../../imagenes/orion.jpg");
-    CImg<unsigned char> img2(img1.width(),img1.height(),1,1);
+
     CImgDisplay disp,disp2;
 
     img1.channel(1);
 
-    cimg_forXY(img1,x,y){
-        img2(x,y) =  a*img1(x,y) + c;
-    }
+    CImg<unsigned char> img2 = lut(img1);
 
     img1.display(disp);
     img2.display(disp2);
