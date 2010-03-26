c8eb2edc8b7abd03c54d57f4933bac2c03f0ab97
esteban <esteban@gravis.(none)>
2010-03-26 18:15:38 -0300

    corregida asignacion de tonos para oscuros y claros. extremos!

--- gtp01/e07/p01.cpp
+++ gtp01/e07/p01.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 using namespace cimg_library;
 
+/**
+   pinta el cuadro segun la tonalidad, 
+   mas puntos negros o blancos segun
+*/
 void pintar( CImg<unsigned char> &cuadro,int tipo ){
     switch(tipo){
+    case 0:
+        // no toca, es negro
     case 1:
         cuadro(0,1) = 255;
     case 2:
@@ -38,42 +44,48 @@ void pintar( CImg<unsigned char> &cuadro,int tipo ){
     case 8:
         cimg_for_insideXY( cuadro, x, y, 1) { cuadro(x,y) = 255; }
         cuadro(1,1) = 0;
-    case 9:
+    case 9: //todo blanco
         cimg_for_insideXY( cuadro, x, y, 1) { cuadro(x,y) = 255; }
     }
 }
 
+/**
+   Asigna un tono de color segun el valor de la imagen
+*/
 CImg<unsigned char> tono( int valor ) {
     CImg<unsigned char> cuadro( 3, 3, 1, 1, 0 );
     int parte = 255 / 10;
 
     if( valor < parte) {
         // si es mayor a 9*parte => no hace nada(todo negro)
-        //pintar( cuadro, 0 ); 
-    } else if( valor < 2*parte) {
         pintar( cuadro, 0 ); 
-    } else if( valor < 3*parte) {
+    } else if( valor < 2*parte) {
         pintar( cuadro, 1 ); 
-    } else if( valor < 4*parte) {
+    } else if( valor < 3*parte) {
         pintar( cuadro, 2 ); 
-    } else if( valor < 5*parte) {
+    } else if( valor < 4*parte) {
         pintar( cuadro, 3 ); 
-    } else if( valor < 6*parte) {
+    } else if( valor < 5*parte) {
         pintar( cuadro, 4 ); 
-    } else if( valor < 7*parte) {
+    } else if( valor < 6*parte) {
         pintar( cuadro, 5 ); 
-    } else if( valor < 8*parte) {
+    } else if( valor < 7*parte) {
         pintar( cuadro, 6 ); 
-    } else if( valor < 9*parte) {
+    } else if( valor < 8*parte) {
         pintar( cuadro, 7 ); 
-    } else if( valor < 10*parte) {
+    } else if( valor < 9*parte) {
         pintar( cuadro, 8 ); 
+    } else if( valor < 10*parte) {
+        pintar( cuadro, 9 ); 
     }
 
 
     return cuadro;
 }
 
+/**
+   Convierte la imagen pasada a una de medios tonos
+*/
  CImg<unsigned char> to_medio_tono( CImg<unsigned char> img ){
 
     CImg<unsigned char> mediotono( 3*img.width(), 3*img.height(), 1, 1 );
@@ -103,11 +115,9 @@ int main( int argc, char **argv ) {
     CImg<unsigned char> mediotono = to_medio_tono( img1 );
 
     mediotono.display(disp2);
-    mediotono.save("mediotono_intermedio.jpg");
 
     mediotono.resize( (int)(mediotono.width()/3), (int)(mediotono.height()/3) );
     mediotono.display();
-    mediotono.save("mediotono.jpg");
 
     while ( !disp.is_closed() && !disp.is_keyQ() ) {}
     return 0;
