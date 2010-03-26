cc7a39796a83b50e523dc14b99f4b92ca769595e
esteban <esteban@gravis.(none)>
2010-03-26 01:47:08 -0300

    agregados cuadros para imagen de medios tonos, hay tres aproximados como ejemplo, falta completar los 10 cuadros

--- gtp01/e07/p01.cpp
+++ gtp01/e07/p01.cpp
@@ -8,24 +8,47 @@ CImg<unsigned char> to_medio_tono( CImg<unsigned char> img, int x, int y ){
 
 }
 
+void pintar( CImg<unsigned char> &cuadro,int tipo ){
+    switch(tipo){
+    case 1:
+        cuadro(1,1) = 255;
+    case 2:
+        cuadro(0,0) = 255;
+        cuadro(1,1) = 255;
+        cuadro(2,2) = 255;
+    case 3:
+        cuadro(0,0) = 255;
+        cuadro(1,1) = 255;
+        cuadro(2,2) = 255;
+        cuadro(1,2) = 255;
+        cuadro(2,1) = 255;
+    }
+}
+
 CImg<unsigned char> tono( int valor ){
     CImg<unsigned char> cuadro( 3, 3, 1, 1, 0 );
-    cuadro(0,0) = 255;
-    cuadro(1,1) = 255;
-    cuadro(2,2) = 255;
+    int parte = 255/3;
+    if( valor < parte) {
+        pintar(cuadro,1); 
+    } else if( valor < 2*parte) {
+        pintar(cuadro,2); 
+    } else if( valor < 3*parte) {
+        pintar(cuadro,3); 
+    }
     return cuadro;
 }
 
 
 int main( int argc, char **argv ) {
 
-    CImg<unsigned char> img1( "../../imagenes/pruebas/pequenia.jpg" );
+    CImg<unsigned char> img1( "../../imagenes/clown.jpg" );
     CImg<unsigned char> mediotono( 3*img1.width(), 3*img1.height(), 1, 1 );
     CImgDisplay disp, disp2;
 
+    img1.display(disp);
 
     cimg_for_insideXY( img1, x, y, 1 ) {
-        CImg<unsigned char> cuadro = tono(2);
+        CImg<unsigned char> cuadro = tono(img1(x,y));
         mediotono( 3*x,   3*y )   = cuadro(0,0);
         mediotono( 3*x,   3*y+1 ) = cuadro(0,1);
         mediotono( 3*x,   3*y+2 ) = cuadro(0,2);
@@ -38,7 +61,7 @@ int main( int argc, char **argv ) {
         //        mediotono.display(disp2);
     }
 
-    img1.display(disp);
+
     mediotono.display(disp2);
     mediotono.save("mediotono.jpg");
 
