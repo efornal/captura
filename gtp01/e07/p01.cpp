f1c1d6a9091fff3031879f44c11a84feafa89850
esteban <esteban@gravis.(none)>
2010-03-26 18:06:58 -0300

    corregida asignacio de cuadros

--- gtp01/e07/p01.cpp
+++ gtp01/e07/p01.cpp
@@ -4,10 +4,6 @@
 using namespace std;
 using namespace cimg_library;
 
-CImg<unsigned char> to_medio_tono( CImg<unsigned char> img, int x, int y ){
-
-}
-
 void pintar( CImg<unsigned char> &cuadro,int tipo ){
     switch(tipo){
     case 1:
@@ -47,42 +43,43 @@ void pintar( CImg<unsigned char> &cuadro,int tipo ){
     }
 }
 
-CImg<unsigned char> tono( int valor ){
+CImg<unsigned char> tono( int valor ) {
     CImg<unsigned char> cuadro( 3, 3, 1, 1, 0 );
-    int parte = 255/10;
+    int parte = 255 / 10;
+
     if( valor < parte) {
-        pintar(cuadro,0); 
+        // si es mayor a 9*parte => no hace nada(todo negro)
+        //pintar( cuadro, 0 ); 
     } else if( valor < 2*parte) {
-        pintar(cuadro,1); 
+        pintar( cuadro, 0 ); 
     } else if( valor < 3*parte) {
-        pintar(cuadro,2); 
+        pintar( cuadro, 1 ); 
     } else if( valor < 4*parte) {
-        pintar(cuadro,3); 
+        pintar( cuadro, 2 ); 
     } else if( valor < 5*parte) {
-        pintar(cuadro,4); 
+        pintar( cuadro, 3 ); 
     } else if( valor < 6*parte) {
-        pintar(cuadro,5); 
+        pintar( cuadro, 4 ); 
     } else if( valor < 7*parte) {
-        pintar(cuadro,6); 
+        pintar( cuadro, 5 ); 
     } else if( valor < 8*parte) {
-        pintar(cuadro,7); 
+        pintar( cuadro, 6 ); 
     } else if( valor < 9*parte) {
-        pintar(cuadro,8); 
+        pintar( cuadro, 7 ); 
+    } else if( valor < 10*parte) {
+        pintar( cuadro, 8 ); 
     }
+
+
     return cuadro;
 }
 
+ CImg<unsigned char> to_medio_tono( CImg<unsigned char> img ){
 
-int main( int argc, char **argv ) {
+    CImg<unsigned char> mediotono( 3*img.width(), 3*img.height(), 1, 1 );
 
-    CImg<unsigned char> img1( "../../imagenes/clown.jpg" );
-    CImg<unsigned char> mediotono( 3*img1.width(), 3*img1.height(), 1, 1 );
-    CImgDisplay disp, disp2;
-
-    img1.display(disp);
-
-    cimg_for_insideXY( img1, x, y, 1 ) {
-        CImg<unsigned char> cuadro = tono(img1(x,y));
+    cimg_for_insideXY( img, x, y, 1 ) {
+        CImg<unsigned char> cuadro = tono(img(x,y));
         mediotono( 3*x,   3*y )   = cuadro(0,0);
         mediotono( 3*x,   3*y+1 ) = cuadro(0,1);
         mediotono( 3*x,   3*y+2 ) = cuadro(0,2);
@@ -92,11 +89,24 @@ int main( int argc, char **argv ) {
         mediotono( 3*x+2, 3*y )   = cuadro(2,0);
         mediotono( 3*x+2, 3*y+1 ) = cuadro(2,1);
         mediotono( 3*x+2, 3*y+2 ) = cuadro(2,2);
-        //        mediotono.display(disp2);
     }
+    return mediotono;
+}
 
+int main( int argc, char **argv ) {
+
+    CImg<unsigned char> img1( "../../imagenes/clown.jpg" );
+    CImgDisplay disp, disp2;
+
+    img1.display(disp);
+
+    CImg<unsigned char> mediotono = to_medio_tono( img1 );
 
     mediotono.display(disp2);
+    mediotono.save("mediotono_intermedio.jpg");
+
+    mediotono.resize( (int)(mediotono.width()/3), (int)(mediotono.height()/3) );
+    mediotono.display();
     mediotono.save("mediotono.jpg");
 
     while ( !disp.is_closed() && !disp.is_keyQ() ) {}
