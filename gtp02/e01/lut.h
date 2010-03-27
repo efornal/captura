abc73a6317d56a3217b2a04ea2343a91ff9aec98
esteban <esteban@gravis.(none)>
2010-03-27 12:28:02 -0300

    implamentacion de lut por tramos

--- gtp02/e01/lut.h
+++ gtp02/e01/lut.h
@@ -7,10 +7,39 @@ CImg<unsigned char> lut( CImg<unsigned char> img1, int a=1, int c=0 ) {
     CImg<unsigned char> img2( img1.width(), img1.height(), 1, 1 );
     cimg_forXY( img1, x, y ) {
         img2(x,y) =  a*img1(x,y) + c;
-        if ( (a*img1(x,y) + c) > 255 ) img2(x,y) = 255; // clipping
-        if ( (a*img1(x,y) + c) < 1   ) img2(x,y) = 0; // clipping
+        if ( img2(x,y) > 255 ) img2(x,y) = 255; // clipping
+        if ( img2(x,y) < 1   ) img2(x,y) = 0; // clipping
     }
     return img2; 
+}
+
+/**
+   s=r*factor en el rango x1 <= x <= x2
+*/
+CImg<unsigned char> lut_x_tramo( CImg<unsigned char> img1, int x1, int x2 ) {
+    int factor = 2;
+    CImg<unsigned char> img2( img1.width(), img1.height(), 1, 1, 0 );
+    cimg_forXY( img1, x, y ) {
+        if ( x < x1 || x > x2 ) img2(x,y) =  img1(x,y);
+        if ( x >= x1 && x <= x2 ) img2(x,y) =  img1(x,y)*factor;
+
+        if ( img2(x,y) > 255 ) img2(x,y) = 255; // clipping
+        if ( img2(x,y) < 1   ) img2(x,y) = 0; // clipping
+    }
+    return img2; 
+}
+
+CImg<unsigned char> mapeo_x_tramo( int x1, int x2 ){
+    unsigned char rojo[] = {255,0,0};
+    CImg<unsigned char> mapeo(255,1,1,1,0);
+    CImg<unsigned char> mapeo_disp(255,255,1,1,0);
+
+    cimg_forX( mapeo, x ){ mapeo(x) = x; }
+
+    CImg<unsigned char> mapeado = lut_x_tramo( mapeo, x1, x2 );
+
+    return mapeo_disp.draw_graph( mapeado, rojo, 1, 1, 1 , 255, 0 );
+
 }
 
 CImg<unsigned char> mapeo( int a, int c ){
--- gtp02/e01/p04.cpp
+++ gtp02/e01/p04.cpp
@@ -1,11 +1,34 @@
 /** 
-TERMINAR:
-modificar la funcion de lut para que en determinados tramos sea
-constante, por ejemplo 
-if (img1(x,y)) esta entre 40 y 90, asignar valor constante 100
-si esta en otro rango, asignar una recta con otra pendiente, y a asi..
-
-   __/
---/
-
+    FIXME: el mapeo por tramos no se ve como deberia ser , porque?
+    NOTA:
+      modificar la funcion de lut para que en determinados tramos sea
+      constante, por ejemplo 
+      if (img1(x,y)) esta entre 40 y 90, asignar valor constante 100
+      si esta en otro rango, asignar una recta con otra pendiente, y a asi..
+         __/
+      --/
 **/
+#include <CImg.h>
+#include <lut.h>
+
+using namespace cimg_library;
+using namespace std;
+
+int main( int argc, char **argv ) {
+    const char *filename = cimg_option( "-f", 
+                                        "../../imagenes/earth.bmp", 
+                                        "ruta archivo imagen" );
+    CImg<unsigned char> img1( filename );
+    CImgDisplay disp, disp2, disp3;
+    int x1 = 100, x2 = 200;
+    CImg<unsigned char> img2 = lut_x_tramo( img1, x1, x2 );
+
+    img1.display(disp);
+    img2.display(disp2);
+    
+    mapeo_x_tramo( x1, x2 ).display(disp3);
+
+    while ( (!disp.is_closed() &&  !disp.is_keyQ()) 
+            && (!disp2.is_closed() &&  !disp2.is_keyQ())) {}
+    return 0;
+}
