992d8247573d96da593d3832833a5d7bb25f10be
esteban <esteban@gravis.(none)>
2010-03-29 17:31:19 -0300

    agregadas operaciones de divicion y multiplicacion de mascara

--- gtp02/e03/operaciones.h
+++ gtp02/e03/operaciones.h
@@ -6,3 +6,20 @@ using namespace std;
 CImg<unsigned char> suma( CImg<unsigned char> img1, CImg<unsigned char> img2 ) { 
     return (img1 + img2) / 2;
 }
+
+
+CImg<unsigned char> enmascarar( CImg<unsigned char> img1, CImg<unsigned char> img2 ) { 
+    CImg<unsigned char> mascara( img1.width(), img1.height(), 1, 1);
+    cimg_forXY(img1,x,y){
+        mascara(x,y) = img1(x,y) * img2(x,y);
+    }
+    return mascara;
+}
+
+CImg<unsigned char> division( CImg<unsigned char> img1, CImg<unsigned char> img2 ) { 
+    CImg<unsigned char> result( img1.width(), img1.height(), 1, 1);
+    cimg_forXY(img1,x,y){
+        result(x,y) = img1(x,y) / img2(x,y);
+    }
+    return result;
+}
