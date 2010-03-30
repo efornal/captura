d2b6f11a2371d59cd0b80f1680b7dc1f66258c38
christian <c.pfarher@gmail.com>
2010-03-30 10:47:13 -0300

    hecho lo del submuestreo pero hay errores no se porque no me anda. me canse de renegar

--- christian_workspace/tp1/fuente/ejer5/ejer5_1y2.cpp
+++ christian_workspace/tp1/fuente/ejer5/ejer5_1y2.cpp
@@ -18,83 +18,30 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	CImg<unsigned char> imagen;
-	imagen.load(
-			"/home/christian/Documentos/universidad/2010/captura/imagenes/rmn.jpg");
+	imagen.load("/home/christian/Documentos/universidad/2010/captura/imagenes/rmn.jpg");
 
-	//imagen.get_columns(0,4).display(); // me devuelve desde la fila 0 hasta la columna 4
+	imagen.display("imagen original");
+	cout<<"Original Size: "<< imagen.width()<< " x "<<imagen.height()<<endl;
 
+	CImg<unsigned char> im_submuestreada((int) imagen.width() / 2, (int) imagen.height() / 2);
 
-	CImg<unsigned char> imagen_submuestreada(imagen); //submuestreo sucesivo con un factor de 2
-	CImg<unsigned char>
-			im1((int) imagen.width() / 2, (int) imagen.height() / 2);
+	cout<<"SubSample Size: " <<im_submuestreada.width()<< " x "<< im_submuestreada.height();
 
-/*
-	int matriz [5][5];
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			matriz[i][j]=j+2;
-			cout<<matriz[i][j]<<"  ";
-		}
-		cout<<endl;
-	}
-	cout<<endl;
-*/
-int x1=0, y1=0;
+	int xn=0, yn=0;
+cout<<endl;
 	cimg_forX(imagen, x){
 		if (!(x%2)) continue;
+		cout<<x<< " ";
+		++xn;
 		cimg_forY(imagen, y){
-			if (!(y%2)) continue;
-			im1(x1,y1)=imagen(x,y);
-			y1++;
-		}
-		x1++;
-	}
-/*	CImg<unsigned char> m(5,5,1,1);
-	cimg_forX(m, x){
-		if (!(x%2)) continue;
-		cimg_forY(m,y){
-			cout<<matriz[x][y]<<"  ";
+			//if (!x%2) break;
+			im_submuestreada(xn,yn)=imagen(x,y++);
+			cout<<y<<"   ";
+			++yn;
 		}
 		cout<<endl;
-	}*/
-	/*
-	int aux = 0;
-	int aux1=0;
-	for (int x = 0; x < (int) ((imagen)._width); ++x) {
-		aux1=0; ++aux;
-		if (x % 2)
-			continue;
-		for (int y = 0; y < (int) ((imagen)._height); y++) {
-			im1(aux, aux1) = imagen(x, y);
-			++aux1;
-			++y;
-		}
 	}
-	im1.display();
-*/
-
-	cout << "alto: " << imagen._width << "          ancho: " << imagen._height;
-
-	/*	const int factor = 2;
-	 imagen_submuestreada.resize((int) (imagen.width() / factor),
-	 (int) (imagen.height() / factor));*/
-
-	//lo de arriba es lo mismo que hacer lo que esta aca abajo:
-
-	//imagen_submuestreada.resize(50, 50).display();
-
-	//resize(100,100) // no tiene efecto alguno sobre la imagen?????-???
-	//resize(300,300)  //agranda a 300 x 300...
-	//resize(50,50)  //la deja de 50 x 50 etc.
-
-	/*	CImgDisplay vent1(imagen, "imagen con parametro 1", 1), vent2(imagen,
-	 "imagen con parametro 0", 0),
-	 vent3(imagen, "imagen sin parametro"), vent4(imagen_submuestreada,
-	 "imagen con resize", 1);
-	 imagen_submuestreada.display("con el mismo factor que la anterior");
-	 while (!vent1.is_closed() && !vent2.is_closed() && !vent3.is_closed()
-	 && !vent4.is_closed()) {
-	 }*/
+im_submuestreada.display();
 
 	return 0;
 }
