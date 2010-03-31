57343d3eb349ed2b85cce56484cfde8a3ab048cb
christian <c.pfarher@gmail.com>
2010-03-31 12:18:24 -0300

    negativo de una imagen y agrego grafica para mostrar la transformacion -- arreglar cerrucho no se porque!

--- christian_workspace/tp2/fuente/ejer1/ejer1_3.cpp
+++ christian_workspace/tp2/fuente/ejer1/ejer1_3.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : tp2.cpp
+// Name        :
 // Author      : Christian Pfarher
 // Version     :
 // Copyright   : GNU Public License
@@ -8,27 +8,19 @@
 
 #include <iostream>
 #include <CImg.h>
+#include "lib.h"
+
 using namespace std;
 using namespace cimg_library;
 
-
-CImg<unsigned char> negativo(CImg<unsigned char> & img1)
-{	//retorna el negativo de una imagen
-	int a=-1;
-	unsigned char c=img1.max(); // maximo de la imagen
-    CImg<unsigned char> img2(img1.width(), img1.height(), 1, 1);
-    cimg_forXY(img2,x,y){
-		img2(x,y)=a*img1(x,y)+c;
-	}
-    return img2;
-}
-
 int main() {
-	CImg <unsigned char> img1("../../imagenes/letras1.tif");
-    CImg<unsigned char> img2 = negativo(img1);
-    CImgDisplay vent1(img1, "imagen original", 1), vent2(img2, "negativo de imagen original", 1);
+	CImg<unsigned char> img1("../../imagenes/letras1.tif");
+	CImg<unsigned char> img2 = negativo(img1);
+	CImgDisplay vent1(img1, "imagen original", 1), vent2(img2,
+			"negativo de imagen original", 1), vent3(obtener_grafica_mapeo(-1,
+			img2.max()), "grafica del negativo", 1);
 
-while(!vent1.is_closed()&& !vent2.is_closed()){}
-
+	while (!vent1.is_closed() && !vent2.is_closed()) {
+	}
 	return 0;
 }
