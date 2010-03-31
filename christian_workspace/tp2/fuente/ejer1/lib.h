24eda6c3be76526492c2f4b1add28283351ab248
christian <c.pfarher@gmail.com>
2010-03-31 15:07:44 -0300

    agrego funcion de grafico por tramos, lut por tramos y modificacion de oclores para no alterar imagen en esacala de greises

--- christian_workspace/tp2/fuente/ejer1/lib.h
+++ christian_workspace/tp2/fuente/ejer1/lib.h
@@ -2,7 +2,14 @@
 using namespace std;
 using namespace cimg_library;
 
-
+int clipp(int valor) {
+	if (valor > 255)
+		return 255;
+	else if (valor < 1)
+		return 0;
+	else
+		return valor;
+}
 CImg<unsigned char> lut(CImg<unsigned char> original, int a, int c, bool clip) {
 	/*	entrada_r imagen de entrada sobre la cual se aplica la transformacion
 	 a: factor de ganancia
@@ -16,13 +23,9 @@ CImg<unsigned char> lut(CImg<unsigned char> original, int a, int c, bool clip) {
 		{
 			valor = a * original(x, y) + c;
 			if (clip) {
-				if (valor > 255) {
-					valor = 255;
-
-				} else if (valor < 0) {
-					valor = 0;
-				}
+				valor = clipp(valor);
 			}
+
 			modificada(x, y) = valor;
 		}
 	return modificada;
@@ -32,20 +35,54 @@ CImg<unsigned char> obtener_grafica_mapeo(int a, int c) {
 	/*retorna en una imagen la grafica de la fucion de mapeo segun los
 	 * coeficientes a y c que se pasan por paremtro
 	 * */
-	unsigned char rojo[] = { 255, 0, 0 };
+	unsigned char blanco[] = { 255, 255, 255 };
 	CImg<unsigned char> mapeo(255, 1, 1, 1, 0);
-	CImg<unsigned char> mapeo_disp(255, 255, 1, 3, 0);
+	CImg<unsigned char> mapeo_disp(255, 255, 1, 1, 0);
 	cimg_forX( mapeo, x ) {
 		mapeo(x) = x; //escala de 0 a 255
 	}
 	CImg<unsigned char> mapeado = lut(mapeo, a, c, false);
-	return mapeo_disp.draw_graph(mapeado, rojo, 1, 1, 1, 255, 0);
+	return mapeo_disp.draw_graph(mapeado, blanco, 1, 1, 1, 255, 0);
 }
 
-CImg<unsigned char> negativo(CImg<unsigned char> & img1)
-{	//retorna el negativo de una imagen
-	int a=-1;
-	unsigned char c=img1.max(); // maximo de la imagen
-    return lut(img1, a, c, 1);
+
+CImg<unsigned char> negativo(CImg<unsigned char> & img1) { //retorna el negativo de una imagen
+	int a = -1;
+	unsigned char c = img1.max(); // maximo de la imagen
+	return lut(img1, a, c, 1);
 }
 
+CImg<unsigned char> lut_tramos(CImg<unsigned char> original, int x0, int x1,
+		bool clip, int factor) {
+	/* compresion lineal por tramos
+	 * factor = factore de compresion o estiramiento segun su valor
+	 *
+	 * */
+	CImg<unsigned char> modificada(original.width(), original.height(), 1, 1);
+	cimg_forXY(original, x, y)
+		{
+			switch (1 + (x > x0) + (x > x1)) {
+			case 1: // esta entre 0 y x1
+				modificada(x, y) = original(x, y);
+				break;
+			case 2: //es mayor que x1 y menor que x2
+				modificada(x, y) = original(x, y) * factor;
+				break;
+			case 3: //es mayor que x2
+				modificada(x, y) = original(x, y);
+				break;
+			}
+		}
+	return modificada;
+}
+
+CImg<unsigned char> obtener_grafica_mapeo_tramos(int x0, int x1, int factor) {
+	unsigned char blanco[] = { 255, 255, 255 };
+	CImg<unsigned char> mapeo(255, 1, 1, 1, 0);
+	CImg<unsigned char> mapeo_disp(255, 255, 1, 1, 0);
+	cimg_forX( mapeo, x ) {
+		mapeo(x) = x; //escala de 0 a 255
+	}
+	CImg<unsigned char> mapeado = lut_tramos(mapeo, x0,x1, 1, factor);
+	return mapeo_disp.draw_graph(mapeado, blanco, 1, 1, 1, 255, 0);
+}
