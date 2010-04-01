223a39fcac0cf1b01b47476a146246d4db16696b
christian <c.pfarher@gmail.com>
2010-04-01 19:57:34 -0300

    ejercicio 3.1 multiplicacion de imagenes funcionando

---
+++ christian_workspace/tp2/fuente/ejer3/ejer3_1_c.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <CImg.h>
+#include "../ejer1/lib.h"
+
+using namespace std;
+using namespace cimg_library;
+//FIXME: que pasa con las imagenes? sale cualqueri cosa ?? coincidera que todo es ruido porque no hay nada que coincide?
+
+int main() {
+
+	CImg<unsigned char> im1, im2;
+
+	im1.load("../../imagenes/img.tif"); //cargo imagenes
+	im2.load("../../imagenes/letras1.tif");
+
+	CImgList <unsigned char> lista1(im1, im2, multiplicar<CImg<unsigned char> > (im1,
+			im2, true)); //armo una lista con las imagenes y la multiplicar de las mismas
+	CImgDisplay disp1(lista1,
+			"multiplica de imagenes letras1 y letras 2 - normalizado"); //muestreo en un display
+
+	CImgList<unsigned char> lista2(im1, im2, multiplicar<CImg<unsigned char> > (im1,
+			im2, false));
+	CImgDisplay disp2(lista2,
+			"multiplica de imagenes letras1 y letras 2 - NO normalizado"); //muestreo en un display
+
+
+	CImgList<unsigned char> lista3(im1, negativo(im1),
+			multiplicar<CImg<unsigned char> > (im1, negativo(im1), true));
+	CImgDisplay disp3(lista3,
+			"multiplica de imagenes letras 1 y su negativo - normalizado"); //muestreo en un display
+
+	CImgList<unsigned char> lista4(im1, negativo(im1),
+			multiplicar<CImg<unsigned char> > (im1, negativo(im1), false));
+	CImgDisplay disp4(lista4,
+			"multiplica de imagenes letras1 y su negativo - NO normalizado"); //muestreo en un display
+
+	while (!disp1.is_closed()) {
+	}
+	return 0;
+}
