cf7a6dad4a27b572d1704dff5b93b0f7d848f32c
christian <c.pfarher@gmail.com>
2010-04-01 17:27:48 -0300

    modifico comentarios y agrego factor que faltaba

--- christian_workspace/tp2/fuente/ejer2/ejer2_2_p.cpp
+++ christian_workspace/tp2/fuente/ejer2/ejer2_2_p.cpp
@@ -5,7 +5,9 @@
 // Copyright   : GNU Public License
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-//FIXME: creo que anda mal, ejecutarlo y ver los resutlados creo que la funcion potencia esta mal...
+//FIXME: creo que anda mal, ejecutarlo y ver los resutlados creo que la funcion potencia esta mal... y
+// ver la grafica porque esta mal!????
+
 #include <iostream>
 #include <CImg.h>
 #include "../ejer1/lib.h"
@@ -17,11 +19,12 @@ int main() {
 	CImg<unsigned char> imagen, im_log;
 	imagen.load("../../imagenes/rmn.jpg");
 	CImgDisplay disp1, disp2;
+	int factor=1;
 	for (int i=0;i<100;i++) {
-		im_log=potencia(imagen, i);
+		im_log=potencia(imagen, i, factor);
 		disp1.set_title("grafica logaritmo variando el factor");
 		im_log.display(disp1);
-		cout<<"Factor: "<<i<<endl;
+		cout<<"exponente: "<<i<<"     Factor: "<<factor<<endl;
 		obtener_grafica_mapeo_potencia(i).display(disp2);
 		while (!disp1.button()){}
 			sleep(1);
