4ae61f3101a9ad346160ebd39a24b1eaa67f7c18
christian <c.pfarher@gmail.com>
2010-04-01 18:28:37 -0300

    cambio nombre de funciones suma y resta segund lib.h que fue actualizado

--- christian_workspace/tp2/fuente/ejer3/ejer3_1_b.cpp
+++ christian_workspace/tp2/fuente/ejer3/ejer3_1_b.cpp
@@ -12,24 +12,24 @@ int main() {
 	im1.load("../../imagenes/letras1.tif"); //cargo imagenes
 	im2.load("../../imagenes/letras2.tif");
 
-	CImgList<unsigned char> lista1(im1, im2, resta<CImg<unsigned char> > (im1,
+	CImgList<unsigned char> lista1(im1, im2, restar<CImg<unsigned char> > (im1,
 			im2, true)); //armo una lista con las imagenes y la resta de las mismas
 	CImgDisplay disp1(lista1,
 			"resta de imagenes letras1 y letras 2 - normalizado"); //muestreo en un display
 
-	CImgList<unsigned char> lista2(im1, im2, resta<CImg<unsigned char> > (im1,
+	CImgList<unsigned char> lista2(im1, im2, restar<CImg<unsigned char> > (im1,
 			im2, false));
 	CImgDisplay disp2(lista2,
 			"resta de imagenes letras1 y letras 2 - NO normalizado"); //muestreo en un display
 
 
-	CImgList<unsigned char> lista3(im1, negativo(im1),
-			resta<CImg<unsigned char> > (im1, negativo(im1), true));
+	CImgList<unsigned char> lista3(im1, negativo(im1), restar<
+			CImg<unsigned char> > (im1, negativo(im1), true));
 	CImgDisplay disp3(lista3,
 			"resta de imagenes letras 1 y su negativo - normalizado"); //muestreo en un display
 
-	CImgList<unsigned char> lista4(im1, negativo(im1),
-			resta<CImg<unsigned char> > (im1, negativo(im1), false));
+	CImgList<unsigned char> lista4(im1, negativo(im1), restar<
+			CImg<unsigned char> > (im1, negativo(im1), false));
 	CImgDisplay disp4(lista4,
 			"resta de imagenes letras1 y su negativo - NO normalizado"); //muestreo en un display
 
