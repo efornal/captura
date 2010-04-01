c959a3cb4ef1f1d71bfdd263ed7e81f106b49cea
christian <c.pfarher@gmail.com>
2010-04-01 18:10:56 -0300

    ejercicio restando imagenes

---
+++ christian_workspace/tp2/fuente/ejer3/ejer3_1_b.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <CImg.h>
+#include "../ejer1/lib.h"
+
+using namespace std;
+using namespace cimg_library;
+//FIXME: que onda con el normalizado?? el tema de los colores que quedan segun lo que sale aca..
+int main() {
+
+	CImg<unsigned char> im1, im2;
+
+	im1.load("../../imagenes/letras1.tif"); //cargo imagenes
+	im2.load("../../imagenes/letras2.tif");
+
+	CImgList<unsigned char> lista1(im1, im2, resta<CImg<unsigned char> > (im1,
+			im2, true)); //armo una lista con las imagenes y la resta de las mismas
+	CImgDisplay disp1(lista1,
+			"resta de imagenes letras1 y letras 2 - normalizado"); //muestreo en un display
+
+	CImgList<unsigned char> lista2(im1, im2, resta<CImg<unsigned char> > (im1,
+			im2, false));
+	CImgDisplay disp2(lista2,
+			"resta de imagenes letras1 y letras 2 - NO normalizado"); //muestreo en un display
+
+
+	CImgList<unsigned char> lista3(im1, negativo(im1),
+			resta<CImg<unsigned char> > (im1, negativo(im1), true));
+	CImgDisplay disp3(lista3,
+			"resta de imagenes letras 1 y su negativo - normalizado"); //muestreo en un display
+
+	CImgList<unsigned char> lista4(im1, negativo(im1),
+			resta<CImg<unsigned char> > (im1, negativo(im1), false));
+	CImgDisplay disp4(lista4,
+			"resta de imagenes letras1 y su negativo - NO normalizado"); //muestreo en un display
+
+	while (!disp1.is_closed()) {
+	}
+	return 0;
+}
