33340df615573db901f5e4cb4f4fa874cbde2433
christian <c.pfarher@gmail.com>
2010-04-01 21:29:47 -0300

    modifico ejercicio para que ande con la nueva funcion modificada en lib.h

--- christian_workspace/tp2/fuente/ejer3/ejer3_1_c.cpp
+++ christian_workspace/tp2/fuente/ejer3/ejer3_1_c.cpp
@@ -13,34 +13,30 @@ int main() {
 	im1.load("../../imagenes/letras1.tif"); //cargo imagenes
 	im2.load("../../imagenes/img.tif");
 
-	CImgList<unsigned char> lista1(im1, im2,
-			multiplicar<CImg<unsigned char> > (im1, im2, true)); //armo una lista con las imagenes y la multiplicar de las mismas
+	CImgList<unsigned char> lista1(im1, im2, multiplicar(im1, im2, true)); //armo una lista con las imagenes y la multiplicar de las mismas
 	CImgDisplay disp1(lista1,
 			"multiplica la imagen letras1 y letras 2 - normalizado"); //muestreo en un display
 
-	CImgList<unsigned char> lista2(im1, im2,
-			multiplicar<CImg<unsigned char> > (im1, im2, false));
+	CImgList<unsigned char> lista2(im1, im2, multiplicar(im1, im2, false));
 	CImgDisplay disp2(lista2,
 			"multiplica la imagen letras1 y letras 2 - NO normalizado"); //muestreo en un display
 
 
-	CImgList<unsigned char> lista3(im1, negativo(im1), multiplicar<CImg<
-			unsigned char> > (im1, negativo(im1), true));
+	CImgList<unsigned char> lista3(im1, negativo(im1), multiplicar(im1,
+			negativo(im1), true));
 	CImgDisplay disp3(lista3,
 			"multiplica la imagen letras 1 y su negativo - normalizado"); //muestreo en un display
 
-	CImgList<unsigned char> lista4(im1, negativo(im1), multiplicar<CImg<
-			unsigned char> > (im1, negativo(im1), false));
+	CImgList<unsigned char> lista4(im1, negativo(im1), multiplicar(im1,
+			negativo(im1), false));
 	CImgDisplay disp4(lista4,
 			"multiplica la imagen letras1 y su negativo - NO normalizado"); //muestreo en un display
 
-	CImgList<unsigned char> lista5(im1, im1,
-			multiplicar<CImg<unsigned char> > (im1, im1, false));
+	CImgList<unsigned char> lista5(im1, im1, multiplicar(im1, im1, false));
 	CImgDisplay disp5(lista5,
 			"multiplica la imagen por si misma - NO normalizado"); //muestreo en un display
 
-	CImgList<unsigned char> lista6(im1, im1, multiplicar<CImg<
-			unsigned char> > (im1, im1, false));
+	CImgList<unsigned char> lista6(im1, im1, multiplicar(im1, im1, false));
 	CImgDisplay disp6(lista6,
 			"multiplica la imagen por si misma - NO normalizado"); //muestreo en un display
 
