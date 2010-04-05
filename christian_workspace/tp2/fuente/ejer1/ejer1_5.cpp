9babf304844c901af9a649f8291128133edbe0d3
christian <c.pfarher@gmail.com>
2010-04-05 15:22:42 -0300

    no entiendo el enunciado, consultar y terminar

---
+++ christian_workspace/tp2/fuente/ejer1/ejer1_5.cpp
@@ -0,0 +1,30 @@
+//FIXME: no entiendo el enunciado
+//============================================================================
+// Author      : Christian Pfarher
+// Version     :
+// Copyright   : GNU Public License
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <CImg.h>
+#include "lib.h"
+#include <string.h>
+
+using namespace std;
+using namespace cimg_library;
+
+int main() {
+	CImg<unsigned char> img(50, 50, 1, 1); //imagen binaria
+
+	unsigned int valor = 0; //negro
+	cimg_forXY(img, x,y)
+		{
+			img(x, y) = valor;
+			valor = !valor;
+		}
+
+	img.normalize(0, 255); //imagen normalizada
+	img.display();
+	return 0;
+}
