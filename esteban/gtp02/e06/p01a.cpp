99c2e8f2aa7ff9b74f3faf25e1b81c80090eb9fd
esteban <efornal@gmail.com>
2010-04-04 21:44:18 -0300

    grafica de rodajas del plano de bits mediante una imagen con escala de grises, se aprecia mejor la posicion de los bits

--- esteban/gtp02/e06/p01a.cpp
+++ esteban/gtp02/e06/p01a.cpp
@@ -1,5 +1,26 @@
+/**
+   Con la imagen en escala de grises se aprecia como interpreta los bits:
+   dec  | bin
+   0    | 0000 0000
+   1    | 0000 0001
+   ...  | 0...
+   127  | 0111 1111
+   ----------------
+   128  | 1000 0000
+   ...  | 1...
+   255  | 1111 1111
+   ________________
+   pos  : 7654 3210
+   
+   para la posicion 7 (bit7) - ver en tabla a lo vertical
+      de 0-127 son todos ceros
+      de 128-255 son todos unos
+   por tanto la imagen correspondiente a este plano es una
+   mitad blanca mitad negra. y asi con el resto.
+*/
 #include <CImg.h>
 #include <plano_bit.h>
+#include <imagenes.h>
 
 using namespace cimg_library;
 using namespace std;
@@ -7,9 +28,9 @@ using namespace std;
 int main( int argc, char **argv ) {
     const char *filename1 = cimg_option( "-f", "../../../imagenes/tablero.png", 
                                         "ruta archivo imagen" );
-
-    CImg<unsigned char> img1( filename1 );
-    img1.display();
+    
+    CImg<unsigned char> img1 = tonos_de_gris( 255 );
+    //    CImg<unsigned char> img1 ( filename );
     
     CImgDisplay disp  ( plano_de_bit( img1, 0 ), "plano 0", 1 );
     CImgDisplay disp1 ( plano_de_bit( img1, 1 ), "plano 1", 1 );
@@ -19,7 +40,9 @@ int main( int argc, char **argv ) {
     CImgDisplay disp5 ( plano_de_bit( img1, 5 ), "plano 5", 1 );
     CImgDisplay disp6 ( plano_de_bit( img1, 6 ), "plano 6", 1 );
     CImgDisplay disp7 ( plano_de_bit( img1, 7 ), "plano 7", 1 );
-    
+
+    img1.display();
+
     while ( (!disp.is_closed() &&  !disp.is_keyQ()) ) {}
     return 0;
 }
