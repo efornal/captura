1e064cd4be618d21eb2ca7f88fd9299a5ef7f9bc
christian <c.pfarher@gmail.com>
2010-04-06 18:50:44 -0300

    agrego comentario sobre que es lo que hace segun interprete en consulta

--- christian_workspace/tp2/fuente/lib/plano_bit.h
+++ christian_workspace/tp2/fuente/lib/plano_bit.h
@@ -9,15 +9,23 @@
 
 using namespace cimg_library;
 
-CImg<unsigned char> plano_de_bit( CImg<unsigned char> img, int nro=0 ){
-    if ( nro > 0 ) {
-        cimg_forXY(img,x,y){
-            img(x,y) = (img(x,y) >> nro) &0x00000001;
-        }
-    } else{
-        cimg_forXY(img,x,y){
-            img(x,y) = (img(x,y)) &0x00000001;
-        }
-    }
-    return img;
+/* Cuando hago lo de los planos de bits en realidad lo que estoy haciendo al ir corriendo el bit es
+ * ir aplicando un umbral (1, 2, 4, 8, 16, 32, 64.... etc.) cuantizado en x niveles 2^x...
+ * Obviamente que con los bits mas significativos por ejemplo 1000 0000 = 64 voy a tener mayor detalle que
+ * si utilizo 01000 0000 =32 por tener menos niveles de cuantizacion y por tanto menos niveles de grises a representar.
+ * */
+CImg<unsigned char> plano_de_bit(CImg<unsigned char> img, int nro = 0) {
+
+	if (nro > 0) {
+		cimg_forXY(img,x,y)
+			{
+				img(x, y) = (img(x, y) >> nro) & 0x00000001;
+			}
+	} else {
+		cimg_forXY(img,x,y)
+			{
+				img(x, y) = (img(x, y)) & 0x00000001;
+			}
+	}
+	return img;
 }
