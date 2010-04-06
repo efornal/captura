fc5c8d9d302aa0991a14cea1d760bba79a5dadcb
christian <c.pfarher@gmail.com>
2010-04-06 11:55:13 -0300

    funcion para el ejercicio 6_1

---
+++ christian_workspace/tp2/fuente/lib/plano_bit.h
@@ -0,0 +1,23 @@
+/*
+ * plano_bit.h
+ *
+ *  Created on: 06/04/2010
+ *      Author: chaco
+ */
+
+#include <CImg.h>
+
+using namespace cimg_library;
+
+CImg<unsigned char> plano_de_bit( CImg<unsigned char> img, int nro=0 ){
+    if ( nro > 0 ) {
+        cimg_forXY(img,x,y){
+            img(x,y) = (img(x,y) >> nro) &0x00000001;
+        }
+    } else{
+        cimg_forXY(img,x,y){
+            img(x,y) = (img(x,y)) &0x00000001;
+        }
+    }
+    return img;
+}
