4c6aed1756673cc0d586e5ac78bef73c8bdd48bc
christian <c.pfarher@gmail.com>
2010-04-06 00:16:05 -0300

    agrego libreria para operadores logicos del ejercicio 5.2

---
+++ christian_workspace/tp2/fuente/lib/operadores_logicos.h
@@ -0,0 +1,51 @@
+/*
+ * operadores_logicos.h
+ *
+ *  Created on: 05/04/2010
+ *      Author: Esteban
+ */
+
+#include <CImg.h>
+
+using namespace cimg_library;
+
+CImg<unsigned char> NOT(CImg<unsigned char> img) {
+	cimg_forXY( img, x, y )
+		{
+			img(x, y) /= 255; // (=0 || =1)
+			img(x, y) = not (img(x, y)) * 255; // (=0 || 255)
+		}
+	return img;
+}
+
+CImg<unsigned char> AND(CImg<unsigned char> img1, CImg<unsigned char> img2) {
+	cimg_forXY( img1, x, y )
+		{
+			img1(x, y) = (img1(x, y) / 255 && img2(x, y) / 255) * 255;
+		}
+	return img1;
+}
+
+CImg<unsigned char> OR(CImg<unsigned char> img1, CImg<unsigned char> img2) {
+	cimg_forXY( img1, x, y )
+		{
+			img1(x, y) = ((img1(x, y) / 255) || (img2(x, y) / 255)) * 255;
+		}
+	return img1;
+}
+
+CImg<unsigned char> XOR(CImg<unsigned char> img1, CImg<unsigned char> img2) {
+	cimg_forXY( img1, x, y )
+		{
+			img1(x, y) = (img1(x, y) / 255 ^ img2(x, y) / 255) * 255;
+		}
+	return img1;
+}
+
+CImg<unsigned char> NOTAND(CImg<unsigned char> img1, CImg<unsigned char> img2) {
+	cimg_forXY( img1, x, y )
+		{
+			img1(x, y) = (!(img1(x, y) / 255) && img2(x, y) / 255) * 255;
+		}
+	return img1;
+}
