178800f495081b9ae68c002a34c7d418554cccba
christian <c.pfarher@gmail.com>
2010-04-07 11:44:43 -0300

    cambio codigo para que funcione con tecla arriba y abajo y derecha e izquierda en vez de con el mouse

--- christian_workspace/tp2/fuente/ejer2/ejer2_2_p.cpp
+++ christian_workspace/tp2/fuente/ejer2/ejer2_2_p.cpp
@@ -16,18 +16,36 @@ using namespace std;
 using namespace cimg_library;
 
 int main() {
-	CImg<unsigned char> imagen, im_log;
+	CImg<unsigned char> imagen, im_pot;
 	imagen.load("../../imagenes/rmn.jpg");
 	CImgDisplay disp1, disp2;
-	int factor=1;
-	for (int i=0;i<100;i++) {
-		im_log=potencia(imagen, i, factor);
+	int i = 1;
+	int factor = 1;
+	im_pot = potencia(imagen, i, factor);
+	disp1.set_title("grafica potencia variando el factor");
+	im_pot.display(disp1);
+	cout << "Factor: " << factor << endl;
+
+	obtener_grafica_mapeo_potencia(i).display(disp2);
+
+	while ((!disp1.is_closed() && !disp1.is_keyQ())) {
+		disp1.wait();
+		if (disp1.is_keyARROWDOWN())
+			i = clipp(--i); // el i es el exponente
+		else if (disp1.is_keyARROWUP()) {
+			i = clipp(++i);
+		} else if (disp1.is_keyARROWLEFT()) //factor
+		{
+			factor = clipp(--factor);
+		} else if (disp1.is_keyARROWRIGHT()) {
+			factor = clipp(++factor);
+		}
+
+		im_pot = potencia(imagen, i, factor);
 		disp1.set_title("grafica logaritmo variando el factor");
-		im_log.display(disp1);
-		cout<<"exponente: "<<i<<"     Factor: "<<factor<<endl;
+		im_pot.display(disp1);
+		cout << "exponenete: " << i << "        - FACTOR: (original^i)*factor : " << factor << endl;
 		obtener_grafica_mapeo_potencia(i).display(disp2);
-		while (!disp1.button()){}
-			sleep(1);
 	}
 	return 0;
 }
