d6ccffc879022f8088572041a435521d0cf9c4c4
esteban <efornal@gmail.com>
2010-04-07 01:07:04 -0300

    corregidos path de imagen y visualizacion parametro en consola

--- esteban/gtp02/e01/p02a.cpp
+++ esteban/gtp02/e01/p02a.cpp
@@ -1,13 +1,14 @@
 #include <CImg.h>
 #include <lut.h>
 #include <stdio.h>
+#include <iostream>
 
 using namespace cimg_library;
 using namespace std;
 
 int main( int argc, char **argv ) {
  const char *filename = cimg_option( "-f", 
-                                        "../../imagenes/earth.bmp", 
+                                        "../../../imagenes/earth.bmp", 
                                         "ruta archivo imagen" );
     int c = 0;
     CImg<unsigned char> img1( filename );
@@ -18,6 +19,7 @@ int main( int argc, char **argv ) {
     for( int a=0; a<255; a++ ){
         lut( img1, a, c ).display(disp2);
         mapeo( a, c ).display(disp3);
+        cout << " a = " << a << endl;
         sleep(1);
     }
 
