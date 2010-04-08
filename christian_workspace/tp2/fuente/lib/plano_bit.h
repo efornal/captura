643338d2bf1141038d04c225149ceb39188e9e93
christian <c.pfarher@gmail.com>
2010-04-07 23:19:42 -0300

    agrego funcion que reconstruye la imagen a partir del plano y hasta el plano que se le pase como parametro

--- christian_workspace/tp2/fuente/lib/plano_bit.h
+++ christian_workspace/tp2/fuente/lib/plano_bit.h
@@ -30,8 +30,8 @@ CImg<unsigned char> plano_de_bit(CImg<unsigned char> img, int nro = 0) {
 	}
 
 	/* 0   ==  0000 0000     => 0 >> 1   =  0000 0000  ==  0
-	   2   ==  0000 0010     => 2 >> 1   =  0000 0001  ==  1
-	   & -> operador and bit a bit o bitand
+	 2   ==  0000 0010     => 2 >> 1   =  0000 0001  ==  1
+	 & -> operador and bit a bit o bitand
 	 * */
 	return img;
 }
@@ -44,3 +44,26 @@ int clipp_plano(int valor) {
 	else
 		return valor;
 }
+
+CImg<unsigned char> get_until_plan(CImg<unsigned char> imagen,
+		int desde_este_plano = 0, int hasta_este_plano = 7) {
+	/*retorna la imagen generada desde el plano: desde_este_plano hasta el plano: hasta_este_plano, (inclusive extremos)
+	 * por defecto retorna la reconstruccion completa de la imagen desde el plano 0 al 7.
+	 */
+	desde_este_plano = clipp_plano(desde_este_plano); //clipea por si me pasa mal lo valores
+	hasta_este_plano = clipp_plano(hasta_este_plano);
+
+	CImg<unsigned char> planos[8];
+	for (int i = 0; i <= 7; i++) {
+		planos[i] = plano_de_bit(imagen, i); //obtengo los 8 planos de 0 a 7 de la imagen
+	}
+
+	CImg<unsigned char> resultado(planos[desde_este_plano]);
+	//etapa de reconstruccion:
+	++desde_este_plano;
+	for (int i = desde_este_plano; i <= hasta_este_plano; i++) {
+		resultado += planos[i] * pow(2, i);
+	}
+	return resultado;
+}
+
