5fa231f0042e71beff7d66f9f09fcaa0d580d23f
christian <c.pfarher@gmail.com>
2010-04-09 17:08:26 -0300

    agrego y borro comentarios... aclaracion sobre la media=0

--- christian_workspace/tp2/fuente/ejer3/ejer3_2.cpp
+++ christian_workspace/tp2/fuente/ejer3/ejer3_2.cpp
@@ -19,11 +19,19 @@ int main(int argc, char **argv) {
 	 * 	en los parámetros se especifica el tipo de ruido (gaussiano es el default) y la varianza.
 	 * La media del ruido generado es cero. El ruido generado es aditivo.*/
 
+	/*POSTA:
+	 * para ver si la media del ruido es cero lo que tenes que hacer es sacar el
+	 * brillo medio de la imagen ( promedio de todos los pixels ) y desp le agregas
+	 * ruido haces de nuevo el proceso y te fijas si no cambia la media el ruido tiene
+	 * media cero
+	 * */
+
 	CImg<float> imagen_limpia;
 	imagen_limpia.load("../../imagenes/tablero.png");
 	double varianza = 0.05;
 	CImgDisplay disp1, disp2, disp3;
 	imagen_limpia.display(disp1);
+	cout<<"Media de la imagen original: "<<imagen_limpia.mean()<<endl;
 	disp1.set_title("imagen original");
 
 	/* Add noise to the image.
@@ -33,22 +41,25 @@ int main(int argc, char **argv) {
 	 Returns:
 	 A noisy version of the instance image.
 	 */
+
 	int cantidad_cuadros = 60;
 	CImg<float> imagen_sucia[cantidad_cuadros];
 	for (int i = 0; i < cantidad_cuadros; i++) {
-		imagen_sucia[i] = imagen_limpia.get_noise(varianza); //FIXME: esta bien esto? com ohago para que el prpmedio me de 0 exacto?
+		imagen_sucia[i] = imagen_limpia.get_noise(varianza);
 	}
 	imagen_sucia[10].display(disp3);
 	imagen_sucia[10].print();
+	cout<<"Media de la imagen con ruido: "<<imagen_sucia[10].mean()<<endl;
 
-	disp3.set_title("imagen sucia 10");
+	disp3.set_title("imagen sucia 10"); //FIXME: el ruido no se nota porque es muy chica la varianza?
 	CImg<float> reconstruida = imagen_sucia[0];
 	for (int i = 1; i < cantidad_cuadros; i++) {
 		reconstruida = sumar<CImg<float> > (reconstruida, imagen_sucia[i]);
 	}
 	cimg_forXY(reconstruida, x, y)
 		{
-			reconstruida(x, y) /= 0.01; //FIXME: si le pongo 1/100 me tira error como que esta muy cerca de 0;
+			reconstruida(x, y) = reconstruida(x, y)/(0.01666); // 60=cant cuadros si pongo FIXME:1/60 me lo redondea a 0.. ni siguiera anda casteandolo!
+
 		}
 	reconstruida.display(disp2);
 	disp2.set_title("reconstruida con 70 cuadros");
