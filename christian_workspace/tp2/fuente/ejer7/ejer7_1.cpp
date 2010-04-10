7c038b62b558dfe03dc7999746f28ed74f7ac8ff
christian <c.pfarher@gmail.com>
2010-04-10 18:39:32 -0300

    ejercicio 7_1 andando... se ve el resultado usando las teclas

--- christian_workspace/tp2/fuente/ejer7/ejer7_1.cpp
+++ christian_workspace/tp2/fuente/ejer7/ejer7_1.cpp
@@ -12,11 +12,42 @@
 using namespace std;
 using namespace cimg_library;
 
+struct punto {
+	int x;
+	int y;
+};
+
 int main(int argc, char **argv) {
-	//TODO: ver funcion get_noise
-	/* generar una imagen agregarle ruido y hacer varias imagenes para reconstruir... y ver hasta cuantas imagenes necesito
-	 * pare reconstrubir
-	 *
-	 * */
-return 0;
+	CImg<unsigned char> imagen;
+	float factor = 0;
+	imagen.load("../../imagenes/earth.bmp");
+	// si se ejecuta el ejercicio ejer1/ejer1_2_a.cpp... la imagen aparece en las coordenadas 447, 270 aprox.
+	CImgDisplay disp1, disp2;
+	imagen.display(disp1);
+	disp1.set_title("imagen original");
+
+	punto primero;
+	punto segundo;
+	primero.x = 447;
+	primero.y = 0; // guarda esto medio que no se entiende pero es simplemente porque esta cambiado los ejes recordar que el
+	//eje y empieza en la esquina superior izquierda y aumenta hacia abajo de la pantalla....
+	segundo.x = imagen.width();
+	segundo.y = 270;
+	CImg<unsigned char> im_mod = lut_tramos2dimensiones<CImg<unsigned char> > (
+			imagen, primero.x, primero.y, segundo.x, segundo.y, true, factor);
+	im_mod.display(disp2);
+	disp2.set_title("imagen modificada variando el factor - presiones las teclas hacia arriba y abajo para variarlo...");
+
+	while (!disp2.is_closed()) {
+		if (disp2.is_keyARROWUP()) {
+			factor = clipp<float> (++factor);
+		} else if (disp2.is_keyARROWDOWN()) {
+			factor = clipp<float> (--factor);
+		}
+		im_mod = lut_tramos2dimensiones(imagen, primero.x, primero.y,
+				segundo.x, segundo.y, true, factor);
+		cout << "factor: " << factor << endl;
+		im_mod.display(disp2);
+	}
+	return 0;
 }
