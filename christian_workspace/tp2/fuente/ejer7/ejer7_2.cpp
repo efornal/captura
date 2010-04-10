163be89f29688e6d04f41171a3dd36e146e1cff7
christian <c.pfarher@gmail.com>
2010-04-10 19:52:07 -0300

    ejercico 7 2 de identificacion de placas andando

--- christian_workspace/tp2/fuente/ejer7/ejer7_2.cpp
+++ christian_workspace/tp2/fuente/ejer7/ejer7_2.cpp
@@ -8,26 +8,42 @@
 #include <iostream>
 #include <CImg.h>
 #include "../lib/lib.h"
+#include "../lib/operadores_logicos.h"
 
 using namespace std;
 using namespace cimg_library;
 
 int main(int argc, char **argv) {
-	CImg < unsigned char > img_a7v600x, img_a7v600se;
-	img_a7v600x.load("a7v600-X.gif");
-	img_a7v600se.load("a7v600-SE.gif");
-	img_a7v600se.display("se");
-	img_a7v600x.display("x");
 
-	//binarizo la imagen, hago un and entre ambas y si me da 1 siginifica que son iguales
+	// se podria hacer tambien detectando o aplicando el mismo algoritmo
+	// sobre solo una parte...
 
-	CImg < unsigned char > img_entrada1, img_entrada2; // es la imagen la cual supongo no voy a saber cual es..
-	/*img_entrada1 (img_a7v600x);
-	img_entrada2 (img_a7v600se);*/
+	// Tambien se podria tomar el valor del pixel en cierto lugar y si es uno u otro
+	// ya sabes que placa es... variantes hay muchas...
 
+	CImg<unsigned char> img_a7v600x, img_a7v600se;
+	img_a7v600x.load("../../imagenes/a7v600-X.gif");
+	img_a7v600se.load("../../imagenes/a7v600-SE.gif");
 
+	CImgList<unsigned char> lista(img_a7v600x, img_a7v600se);
+	lista.display("X y SE respectivamente");
 
+	CImg<unsigned char> entrada_a7v600x(img_a7v600x);
 
+	CImgList <unsigned char> result1(img_a7v600se, entrada_a7v600x);
+	if (intensidades_iguales<CImg<unsigned char> > (img_a7v600se,
+			entrada_a7v600x)){
+		result1.display("son iguales");
+	}
+	else
+		result1.display("son diferentes");
 
-return 0;
+	CImgList <unsigned char> result2(img_a7v600x, entrada_a7v600x);
+		if (intensidades_iguales<CImg<unsigned char> > (img_a7v600x,
+				entrada_a7v600x)){
+			result2.display("son iguales");
+		}
+		else
+			result2.display("son diferentes");
+	return 0;
 }
