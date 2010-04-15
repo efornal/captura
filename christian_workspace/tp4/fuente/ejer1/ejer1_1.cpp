01285e538ffa4c0886deb2a84b07cf178df90f0e
christian <c.pfarher@gmail.com>
2010-04-15 16:57:15 -0300

    ejercicio 1 1 terminado

--- christian_workspace/tp4/fuente/ejer1/ejer1_1.cpp
+++ christian_workspace/tp4/fuente/ejer1/ejer1_1.cpp
@@ -12,27 +12,58 @@ using namespace std;
 using namespace cimg_library;
 
 int main(int argc, char **argv) {
-	CImg<unsigned char> irgb_0, irgb_1, irgb_2, imagen_rgb;
-	CImg<unsigned char> ihsi_h, ihsi_s, ihsi_i, imagen_hsi;
+	CImg<unsigned char> imagen_rgb;
+	CImg<double> imagen_hsi;
 
 	imagen_rgb.load("../../imagenes/patron.tif");
 
 	CImgDisplay disp1, disp2;
-	irgb_0.channel(0) = imagen_rgb.get_channel(0); //rojo
-	irgb_1.channel(1) = imagen_rgb.get_channel(1); //azul
-	irgb_2.channel(2) = imagen_rgb.get_channel(2); //verde
 
-	CImgList<unsigned char> lista(imagen_rgb, irgb_0.channel(0), irgb_1, irgb_2);
-	lista.display(disp1);
-	disp1.set_title("rgb");
+	CImg<unsigned char> irgb_0(imagen_rgb);
+	CImg<unsigned char> irgb_1(imagen_rgb);
+	CImg<unsigned char> irgb_2(imagen_rgb);
+
+	cimg_forXY(imagen_rgb, x,y)
+		{
+			irgb_0(x, y, 0, 0) = imagen_rgb(x, y, 0, 0);
+			irgb_0(x, y, 0, 1) = 0;//pongo en cero el canal verde
+			irgb_0(x, y, 0, 2) = 0; //pongo en cero el canal azul
+
+			irgb_1(x, y, 0, 0) = 0;//pongo en cero el canal rojo
+			irgb_1(x, y, 0, 1) = imagen_rgb(x, y, 0, 1);
+			irgb_1(x, y, 0, 2) = 0;//pongo en cero el canal azul
+
+			irgb_2(x, y, 0, 0) = 0; //pongo en cero el canal rojo
+			irgb_2(x, y, 0, 1) = 0; //pongo en cero el canal verde
+			irgb_2(x, y, 0, 2) = imagen_rgb(x, y, 0, 2);
+		}
+
+	CImgList<unsigned char> lista(imagen_rgb, irgb_0, irgb_1, irgb_2);
+	lista.display(disp1, 'y', 'c');
+
+	disp1.set_title("imgagen rgb y sus respectivos canales");
+
 	imagen_hsi = imagen_rgb.get_RGBtoHSI();
-	/*ihsi_h = imagen_hsi.get_channel(0); //obtengo canal h
-	ihsi_s = imagen_hsi.get_channel(1); //obtengo canal s
-	ihsi_i = imagen_hsi.get_channel(2); //obtengo canal i
+	CImg <double> ihsi_h(imagen_rgb.width(), imagen_rgb.height(), 1, 3, 0);
+	CImg <double> ihsi_s(imagen_rgb.width(), imagen_rgb.height(), 1, 3, 0);
+	CImg <double> ihsi_i(imagen_rgb.width(), imagen_rgb.height(), 1, 3, 0);
 
-	CImgList<float> lista1(imagen_hsi, ihsi_h, ihsi_i, ihsi_s);
-	lista1.display(disp2);
-	disp2.set_title("hsi");*/
-	while (!disp1.is_closed()){}
+	cimg_forXY(imagen_hsi, x,y)
+			{
+		/*POSTA: el H va entre 0 y 360 y S e I van de 0 a 1 com ola lista normaliza si
+		 * no hago lo que hice abajo se ve el s y el i como _todo negro.... por eso hago
+		 * eso aca abajo
+		 * */
+		ihsi_h(x, y, 0, 0) = (imagen_hsi(x, y, 0, 0)/360)*255;
+		ihsi_s(x, y, 0, 1) = imagen_hsi(x, y, 0, 1)*255;
+		ihsi_i(x, y, 0, 2) = imagen_hsi(x, y, 0, 2)*255;
+			}
+
+	CImgList<double> lista1(imagen_hsi, ihsi_h, ihsi_s, ihsi_i);
+	lista1.display(disp2, 'y', 'c');
+
+	disp2.set_title("hsi");
+	while (!disp1.is_closed()) {
+	}
 	return 0;
 }
