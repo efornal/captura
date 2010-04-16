d276cf80e8c88826b42637a77a574b53e4e62681
christian <c.pfarher@gmail.com>
2010-04-16 17:53:37 -0300

    agrego una mascara no simetrica

--- christian_workspace/tp3/fuente/lib/mask.h
+++ christian_workspace/tp3/fuente/lib/mask.h
@@ -12,7 +12,6 @@ using namespace std;
 using namespace cimg_library;
 
 template<class T>
-
 CImg<T> mascara3x3() {
 	CImg<T> imagen (3,3,1,1);
 	imagen(0, 0) = 1 * (1 / 9.0);
@@ -24,5 +23,20 @@ CImg<T> mascara3x3() {
 	imagen(2, 0) = 1 * (1 / 9.0);
 	imagen(2, 1) = 1 * (1 / 9.0);
 	imagen(2, 2) = 1 * (1 / 9.0);
+	return imagen;
+}
+
+template <class T>
+CImg<T> mascara3x3_no_simetrica() {
+	CImg<T> imagen (3,3,1,1);
+	imagen(0, 0) = 1 * (1 / 10.0);
+	imagen(0, 1) = 2 * (1 / 10.0);
+	imagen(0, 2) = 1 * (1 / 10.0);
+	imagen(1, 0) = 1 * (1 / 10.0);
+	imagen(1, 1) = 1 * (1 / 10.0);
+	imagen(1, 2) = 1 * (1 / 10.0);
+	imagen(2, 0) = 1 * (1 / 10.0);
+	imagen(2, 1) = 1 * (1 / 10.0);
+	imagen(2, 2) = 1 * (1 / 10.0);
 	return imagen;
 }
