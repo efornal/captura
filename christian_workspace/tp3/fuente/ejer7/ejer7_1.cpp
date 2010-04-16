fc0985b5a3711ee48207efe84f68fd9cf00186bc
christian <c.pfarher@gmail.com>
2010-04-16 14:58:29 -0300

    agrego ejercicois empezando tp3

---
+++ christian_workspace/tp3/fuente/ejer7/ejer7_1.cpp
@@ -0,0 +1,27 @@
+/*
+ * ejer7_1.cpp
+ *
+ *  Created on: 08/04/2010
+ *      Author: christian
+ */
+
+#include <iostream>
+#include <CImg.h>
+
+
+using namespace std;
+using namespace cimg_library;
+
+int main(int argc, char **argv) {
+	/*TODO: calcular el histograma y solo aplicar al valor del centro...
+	 * tomar imagen vecindad como una nueva imagen.. . cortamos ese pedazo le  hcemos la imagen ecualizada... tomamos le valor delo medio
+	 * y reemplazamos la imagen por eso... y asi sucesivamente
+	 * CImg <unsigned char> res(img);
+	CImg <unsigned char> S(3,3), aux(3,3);
+			cimg_for3x3(img, x, y, 0,0,3);
+			aux=S.get_equalize(nlevels);
+			res(x,y) = aux(1,1);
+	 * */
+
+return 0;
+}
