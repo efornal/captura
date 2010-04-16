73ce44e978bf16aea2f9d0678c7158bc984fbfe6
christian <c.pfarher@gmail.com>
2010-04-16 17:33:14 -0300

    convolucion de una mascara con una imagen

--- christian_workspace/tp3/fuente/ejer2/ejer2_1.cpp
+++ christian_workspace/tp3/fuente/ejer2/ejer2_1.cpp
@@ -7,22 +7,26 @@
 
 #include <iostream>
 #include <CImg.h>
-#include "../lib/mask.h";
+#include "../lib/mask.h"
 
 using namespace std;
 using namespace cimg_library;
 
 int main(int argc, char **argv) {
-	CImg<unsigned char> imagen("../../imagenes/tablero.png");
-	CImg<unsigned char> resultado=imagen.convolve(mascara3x3 <unsigned char> ());
+	CImg<float> imagen("../../imagenes/tablero.png");
 
-	CImgDisplay disp1, disp2, disp3;
+	CImg<float> resultado = imagen.get_convolve(mascara3x3<float> ());
 
-	imagen.display(disp1); disp1.set_title("imagen original");
-	resultado.display(disp2); disp2.set_title("imagen convolucionada con la mascara");
-	mascara3x3 <unsigned char> ().display(disp3);
-	disp3.set_title("mascara");
-
+	CImgDisplay disp1, disp2;
+	imagen.display(disp1);
+	disp1.set_title("imagen original");
+	resultado.display(disp2);
+	disp2.set_title("imagen convolucionada con la mascara");
+	mascara3x3<float> ().normalize(0, 255).display("mascara");
+	/* POSTA: cuando convoluciono la imagen y la mascara con todos los valores iguales
+	 * la imagen queda difusa porque para cada pixel le estas asignando el promedio
+	 * de los vecinos
+	 * */
 	while (!disp1.is_closed()) {
 		disp1.wait();
 	}
