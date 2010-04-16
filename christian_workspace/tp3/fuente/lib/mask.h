941a8d8ef7cb82f1c90951022992a273a9f0ff8c
christian <c.pfarher@gmail.com>
2010-04-16 17:33:26 -0300

    corrigo definicion de la imagen

--- christian_workspace/tp3/fuente/lib/mask.h
+++ christian_workspace/tp3/fuente/lib/mask.h
@@ -14,15 +14,15 @@ using namespace cimg_library;
 template<class T>
 
 CImg<T> mascara3x3() {
-	CImg<T> imagen;
-	imagen(0, 0) = 1 * (1 / 9);
-	imagen(0, 1) = 1 * (1 / 9);
-	imagen(0, 2) = 1 * (1 / 9);
-	imagen(1, 0) = 1 * (1 / 9);
-	imagen(1, 1) = 1 * (1 / 9);
-	imagen(1, 2) = 1 * (1 / 9);
-	imagen(2, 0) = 1 * (1 / 9);
-	imagen(2, 1) = 1 * (1 / 9);
-	imagen(2, 2) = 1 * (1 / 9);
+	CImg<T> imagen (3,3,1,1);
+	imagen(0, 0) = 1 * (1 / 9.0);
+	imagen(0, 1) = 1 * (1 / 9.0);
+	imagen(0, 2) = 1 * (1 / 9.0);
+	imagen(1, 0) = 1 * (1 / 9.0);
+	imagen(1, 1) = 1 * (1 / 9.0);
+	imagen(1, 2) = 1 * (1 / 9.0);
+	imagen(2, 0) = 1 * (1 / 9.0);
+	imagen(2, 1) = 1 * (1 / 9.0);
+	imagen(2, 2) = 1 * (1 / 9.0);
 	return imagen;
 }
