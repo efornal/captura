8bfd8b56ecc88d3eb480912dbace9f5f174b9f6f
esteban <efornal@gmail.com>
2010-04-17 12:35:40 -0300

    ordenada lib de filtros

--- esteban/gtp03/e03/p01.cpp
+++ esteban/gtp03/e03/p01.cpp
@@ -18,21 +18,21 @@ int main( int argc, char **argv ) {
     img.display(disp);
     disp.set_title("imagen original");
 
-    masks::mask(3,3).normalize(0,255).display("mask 3x3");
-    masks::mask(5,5).normalize(0,255).display("mask 5x5");
-    masks::mask(20,20).normalize(0,255).display("mask 20x20");
+    masks::promedio(3,3).normalize(0,255).display("mask 3x3");
+    masks::promedio(5,5).normalize(0,255).display("mask 5x5");
+    masks::promedio(20,20).normalize(0,255).display("mask 20x20");
 
-    img_conv = img.get_convolve( masks::mask(3,3) );
+    img_conv = img.get_convolve( masks::promedio(3,3) );
     img_conv.print("img_conv_3x3");
     img_conv.normalize(0,255).display(disp2);
     disp2.set_title("mask 3x3");
 
-    img_corr = img.get_correlate( masks::mask(5,5) );
+    img_corr = img.get_correlate( masks::promedio(5,5) );
     img_corr.print("img_corr_5x5");
     img_corr.normalize(0,255).display(disp3);
     disp3.set_title("mask 5x5");
 
-    img_corr = img.get_correlate( masks::mask(20,20) );
+    img_corr = img.get_correlate( masks::promedio(20,20) );
     img_corr.print("img_corr_20x20");
     img_corr.normalize(0,255).display(disp3);
     disp3.set_title("mask 20x20");
--- esteban/lib/masks.h
+++ esteban/lib/masks.h
@@ -17,15 +17,33 @@ namespace masks {
         return mask;
     }
 
+    // =============== filtros de suavizado ============
+    // pesos positivos que suman 1
+
+    // =============== filtros promediadores ============
+    // pesos iguales
+
     /**
-       retorna una mascara promediadora sum{wi}/n*m
+       retorna una mascara promediadora tipo media estandar
        *  1 1 1
        *  1 1 1 * 1/9
        *  1 1 1
      */
-    CImg<double> media_estandar() {
+    CImg<double> media_estandar( double k=(1/9.0) ) {
         int x=3, y=3, z=1, c=1;
-        double k = 1/9.0;
+        CImg<double> mask( x, y, z, c, 1);
+        mask *= k;
+        return mask;
+    }
+
+    /**
+       retorna una mascara promediadora en forma de signo mas
+       *  0 1 0
+       *  1 1 1 * 1/5
+       *  0 1 0
+     */
+    CImg<double> signo_mas( double k=(1/5.0) ) {
+        int x=3, y=3, z=1, c=1;
         CImg<double> mask( x, y, z, c, 1);
         mask *= k;
         return mask;
@@ -37,9 +55,8 @@ namespace masks {
        *  2 4 2 * 1/16
        *  1 2 1
      */
-    CImg<float> media_ponderada() {
+    CImg<float> media_ponderada( float k=(1/16.0) ) {
         int x=3, y=3, z=1, c=1;
-        float k = 1/16.0;
         CImg<float> mask( x, y, z, c, 1 );
         mask(1,0) *= 2;
         mask(0,1) *= 2;
@@ -50,6 +67,23 @@ namespace masks {
         return mask;
     }
 
+    /**
+     * retorna una mascara promediadora de tamanio especificado 
+     * m * 1/N^2  N = m*n
+     *  1 1 . 1
+     *  1 1 . 1 * 1/N^2   (N = m*n)
+     *  . . . 1
+     *  1 1 1 1
+     */
+    CImg<double> promedio( int x=3, int y=3 ) {
+        int z=1, c=1;
+        CImg<double> mask( x, y, z, c, 1);
+        return  mask * ( 1.0 / (x*y) );
+    }
+
+    // =============== filtros no simetrico ============
+    // no simetrico si A != A transpuesta
+    
     /**
        retorna una mascara no simetrica
        *  1 3 1                      1 2 1
@@ -64,16 +98,6 @@ namespace masks {
         mask(0,1) = 2; 
         mask(2,1) = 3; 
         return mask;
-    }
-
-    /**
-       retorna una mascara de tamanio especificado 
-       m * 1/N^2  N = m*n
-     */
-    CImg<double> mask( int x=3, int y=3 ) {
-        int z=1, c=1;
-        CImg<double> mask( x, y, z, c, 1);
-        return  mask * ( 1.0 / (x*y) );
     }
 
 }
