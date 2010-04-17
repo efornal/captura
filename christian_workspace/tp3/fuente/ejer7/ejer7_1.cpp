ae6a6f2ab2a50f241120f3eed89d2483958dc268
christian <c.pfarher@gmail.com>
2010-04-17 16:02:38 -0300

    no me anda el cimg_for.. asi que no como cuerno hacerlo

--- christian_workspace/tp3/fuente/ejer7/ejer7_1.cpp
+++ christian_workspace/tp3/fuente/ejer7/ejer7_1.cpp
@@ -8,7 +8,6 @@
 #include <iostream>
 #include <CImg.h>
 
-
 using namespace std;
 using namespace cimg_library;
 
@@ -16,12 +15,27 @@ int main(int argc, char **argv) {
 	/*TODO: calcular el histograma y solo aplicar al valor del centro...
 	 * tomar imagen vecindad como una nueva imagen.. . cortamos ese pedazo le  hcemos la imagen ecualizada... tomamos le valor delo medio
 	 * y reemplazamos la imagen por eso... y asi sucesivamente
-	 * CImg <unsigned char> res(img);
-	CImg <unsigned char> S(3,3), aux(3,3);
-			cimg_for3x3(img, x, y, 0,0,3);
-			aux=S.get_equalize(nlevels);
-			res(x,y) = aux(1,1);
+	 *
 	 * */
+	CImg<unsigned char> imagen("../../imagenes/ej7a.tif");
+	CImgDisplay disp1, disp2, disp3;
+	int nlevels = 255;
+	CImg<unsigned char> imagen_ecualizada = imagen.get_equalize(nlevels);
 
-return 0;
+	CImg<unsigned char> aux(3, 3, 0, 0); //creo una copia de la imagen original
+	CImg<unsigned char> aux1(imagen); //creo una copia de la imagen original
+//FIXME: no me anda el tema de l mascara....
+	cimg_for3x3(aux1, x, y, 0, 0, 0)
+		{
+			aux(x,y)=aux1(x,y).get_equalize(nlevels)(1,1);
+		}
+	CImgList<unsigned char> lista(imagen, imagen_ecualizada, aux1);
+	lista.display(disp1);
+	disp1.set_title(
+			"imagen original - imagen ecualizada - imagen ecualizada por zonas");
+
+	while (!disp1.is_closed()) {
+		disp1.wait();
+	}
+	return 0;
 }
