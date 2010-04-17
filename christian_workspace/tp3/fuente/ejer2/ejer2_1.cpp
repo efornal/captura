7c8cc28c780120d108147aa7a62418d31222e5d1
christian <c.pfarher@gmail.com>
2010-04-17 11:00:59 -0300

    cambio nombre funcion

--- christian_workspace/tp3/fuente/ejer2/ejer2_1.cpp
+++ christian_workspace/tp3/fuente/ejer2/ejer2_1.cpp
@@ -15,14 +15,14 @@ using namespace cimg_library;
 int main(int argc, char **argv) {
 	CImg<float> imagen("../../imagenes/tablero.png");
 
-	CImg<float> resultado = imagen.get_convolve(mascara3x3<float> ());
+	CImg<float> resultado = imagen.get_convolve(generar_mascara3x3<float> ());
 
 	CImgDisplay disp1, disp2;
 	imagen.display(disp1);
 	disp1.set_title("imagen original");
 	resultado.display(disp2);
 	disp2.set_title("imagen convolucionada con la mascara");
-	mascara3x3<float> ().normalize(0, 255).display("mascara");
+	generar_mascara3x3<float> ().normalize(0, 255).display("mascara");
 	/* POSTA: cuando convoluciono la imagen y la mascara con todos los valores iguales
 	 * la imagen queda difusa porque para cada pixel le estas asignando el promedio
 	 * de los vecinos
