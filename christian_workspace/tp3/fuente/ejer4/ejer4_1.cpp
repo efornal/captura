2dc718f4ec57ce6c7a7e1441e590743121fa5622
christian <c.pfarher@gmail.com>
2010-04-17 11:02:31 -0300

    seguri haciendolo porque no me funcion ala formula para guassianas

--- christian_workspace/tp3/fuente/ejer4/ejer4_1.cpp
+++ christian_workspace/tp3/fuente/ejer4/ejer4_1.cpp
@@ -13,33 +13,12 @@ using namespace std;
 using namespace cimg_library;
 
 int main(int argc, char **argv) {
+	//FIXME: no funciona
 	CImg<float> imagen("../../imagenes/tablero.png");
-
-	CImg<float> resultado3 = imagen.get_convolve(generar_mascara<float> (25));
-	CImg<float> resultado15 = imagen.get_convolve(generar_mascara<float> (15));
-	CImg<float> resultado30 = imagen.get_convolve(generar_mascara<float> (30));
-	CImg<float> resultado50 = imagen.get_convolve(generar_mascara<float> (50));
-	/*POSTA: al aumentar el tamanio de la mascara promedias mas valores y por tanto
-	 * vas haciendo cada vez menos notoria la diferencia entre pixeles vecinos y por
-	 * lo tanto resulta en una imagen mas disfuminada */
-	cout<<endl<<"MEdia: " <<resultado3.mean()<<endl<<"varianza"<<resultado3.variance();
-	CImgDisplay disp1, disp2, disp3, disp4, disp5;
-
-	imagen.display(disp1);
-	disp1.set_title("imagen original");
-
-	resultado3.display(disp2);
-	disp2.set_title("imagen aplicando mascara de 3x3");
-
-	resultado15.display(disp3);
-	disp3.set_title("imagen aplicando mascara de 15x15");
-
-	resultado30.display(disp4);
-	disp4.set_title("imagen aplicando mascara de 30x30");
-
-	resultado50.display(disp5);
-	disp5.set_title("imagen aplicando mascara de 50x50");
-
+	CImgDisplay disp1;
+	generar_mascara<double> (0, 0, 10, 10).normalize(0, 255).display();
+	cout << "varianza" << generar_mascara<double> (0, 0, 10, 10).variance()
+			<< endl;
 	while (!disp1.is_closed()) {
 		disp1.wait();
 	}
