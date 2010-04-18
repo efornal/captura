013970e40a46fa0ebb42303b8a83966dc685572f
christian <c.pfarher@gmail.com>
2010-04-17 21:06:07 -0300

    agrego para poder variar el threshold con las teclas

--- christian_workspace/tp3/fuente/ejer4/ejer4_2.cpp
+++ christian_workspace/tp3/fuente/ejer4/ejer4_2.cpp
@@ -14,18 +14,27 @@ using namespace cimg_library;
 
 int main(int argc, char **argv) {
 	CImg<float> imagen("../../imagenes/hubble.tif");
-	CImgDisplay disp1, disp2, disp3;
+	CImgDisplay disp1, disp2, disp3, disp4;
 	imagen.display(disp1);
 	disp1.set_title("imagen original");
 
 	CImg<float> resultado = imagen.get_convolve(generar_mascara<float> (7));
 	resultado.display(disp2);
 	disp2.set_title("imagen filtrada");
+	float threshold=128.0;
 
-	resultado.quantize(2).display(disp3);
-	disp3.set_title("imagen con objetos grandes");
+
 	while (!disp1.is_closed()) {
 		disp1.wait();
+			if (disp1.is_keyARROWUP()){
+				threshold+=1.0;
+			} else if (disp1.is_keyARROWDOWN()){
+				threshold-=1.0;
+			}
+			threshold=clipp <float>(threshold);
+			resultado.get_threshold(threshold).display(disp3);
+			disp3.set_title("imagen con objetos grandes");
+			cout<<"Threshold: "<<threshold<<endl;
 	}
 	return 0;
 }
