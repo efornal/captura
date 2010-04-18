135b66f7626fc94f55cad21e390bcf52ac9ce1a2
christian <c.pfarher@gmail.com>
2010-04-17 21:07:42 -0300

    cambio nombre de archivo

--- christian_workspace/tp4/fuente/ejer1/ejer1_1_a.cpp
+++ christian_workspace/tp4/fuente/ejer1/ejer1_1_a.cpp
@@ -40,30 +40,10 @@ int main(int argc, char **argv) {
 
 	CImgList<unsigned char> lista(imagen_rgb, irgb_0, irgb_1, irgb_2);
 	lista.display(disp1, 'y', 'c');
+	disp1.set_title("imagen rgb y sus respectivos canales");
 
-	disp1.set_title("imgagen rgb y sus respectivos canales");
-
-	imagen_hsi = imagen_rgb.get_RGBtoHSI();
-	CImg <double> ihsi_h(imagen_rgb.width(), imagen_rgb.height(), 1, 3, 0);
-	CImg <double> ihsi_s(imagen_rgb.width(), imagen_rgb.height(), 1, 3, 0);
-	CImg <double> ihsi_i(imagen_rgb.width(), imagen_rgb.height(), 1, 3, 0);
-
-	cimg_forXY(imagen_hsi, x,y)
-			{
-		/*POSTA: el H va entre 0 y 360 y S e I van de 0 a 1 com ola lista normaliza si
-		 * no hago lo que hice abajo se ve el s y el i como _todo negro.... por eso hago
-		 * eso aca abajo
-		 * */
-		ihsi_h(x, y, 0, 0) = (imagen_hsi(x, y, 0, 0)/360)*255;
-		ihsi_s(x, y, 0, 1) = imagen_hsi(x, y, 0, 1)*255;
-		ihsi_i(x, y, 0, 2) = imagen_hsi(x, y, 0, 2)*255;
-			}
-
-	CImgList<double> lista1(imagen_hsi, ihsi_h, ihsi_s, ihsi_i);
-	lista1.display(disp2, 'y', 'c');
-
-	disp2.set_title("hsi");
 	while (!disp1.is_closed()) {
+		disp1.wait();
 	}
 	return 0;
 }
