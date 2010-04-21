14a6eeb0d5897abbd561ef87adfcb4145fc6ee7b
christian <c.pfarher@gmail.com>
2010-04-21 19:10:23 -0300

    ejercicio corregido y andando con la funcion para aplicar paletas

--- christian_workspace/tp4/fuente/ejer3/ejer3_1.cpp
+++ christian_workspace/tp4/fuente/ejer3/ejer3_1.cpp
@@ -16,54 +16,30 @@ using namespace cimg_library;
 
 int main(int argc, char **argv) {
 	CImg<float> imagen(256, 256, 1, 1);
+	CImg<float> la_paleta(256, 256, 1, 1, 255.0);
 
 	cimg_forXY(imagen, x,y)
 		{
 			imagen(x, y) = x; //genero imagen en tonos de grises
 		}
-	/*
-	 * ../../paletas/bone.pal
-	 * ../../paletas/cool.pal
-	 * ../../paletas/copper.pal
-	 * ../../paletas/gray.pal
-	 * ../../paletas/hot.pal
-	 * ../../paletas/hsv.pal
-	 * ../../paletas/jet.pal
-	 * ../../paletas/pink.pal
-	 * */
 
 	vector<vector<float> > paleta; //creo un vector de vectores
 	cargar_paleta(paleta, "../../paletas/bone.pal");
 
 	CImgDisplay disp1, disp2, disp3;
-	CImg<float> imagen_con_paleta_aplicada(imagen.width(), imagen.height(), 1,
-			3);
-	CImg<float> la_paleta(256, 256, 1, 3);
-	//TODO: hacer funcion cargar paleta que lo aplique sobre unaimagen..
 
-	cimg_forXY(imagen_con_paleta_aplicada, x, y)
-		{ //es una lut:
-			imagen_con_paleta_aplicada(x, y, 0, 0) = paleta[imagen(x, y)][0]
-					* 255.0; //canal rojo
-			imagen_con_paleta_aplicada(x, y, 0, 1) = paleta[imagen(x, y)][1]
-					* 255.0; //canal verde
-			imagen_con_paleta_aplicada(x, y, 0, 2) = paleta[imagen(x, y)][2]
-					* 255.0; //canal azul
+	CImg<float> imagen_con_paleta_aplicada=aplicar_paleta(imagen, 1);
+	la_paleta=aplicar_paleta(la_paleta, 1);
 
-			la_paleta(x, y, 0, 0) = paleta[x][0] * 255;
-			la_paleta(x, y, 0, 1) = paleta[x][1] * 255;
-			la_paleta(x, y, 0, 2) = paleta[x][2] * 255;
-		}
-	la_paleta.print();
 	imagen.display(disp1);
 	disp1.set_title("imagen a la que se le aplica la paleta");
 
 	imagen_con_paleta_aplicada.display(disp2);
 	disp2.set_title("imagen con paleta aplicada");
 
-	la_paleta.display(disp3);
-	disp3.set_title("la paleta");
 
+	imagen_con_paleta_aplicada.display(disp3);
+	disp3.set_title("paleta aplicada a una imagen blanca");
 	while (!disp1.is_closed()) {
 		disp1.wait();
 	}
