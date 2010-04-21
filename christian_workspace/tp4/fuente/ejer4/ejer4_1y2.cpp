876dadca08f49701fd63e3a844e74e29a0f411db
christian <c.pfarher@gmail.com>
2010-04-21 18:28:37 -0300

    412 andadno

--- christian_workspace/tp4/fuente/ejer4/ejer4_1y2.cpp
+++ christian_workspace/tp4/fuente/ejer4/ejer4_1y2.cpp
@@ -12,28 +12,36 @@ using namespace std;
 using namespace cimg_library;
 
 int main(int argc, char **argv) {
-	CImg<float> rio("../../imagenes/rio.jpg");
+	CImg<float> rio("../../imagenes/rio.jpg"); //tiene valores entre 0 y 255
 	CImgDisplay disp1(rio, "imagen del rio", 1), disp2, disp3;
 	rio.get_histogram(255).display_graph(disp2, 3);
-	//el color del rio esta entre 0 y 1 inclusive.
-	CImg<float> rio_color(rio.width(), rio.height(), rio.depth(), 3); // rgb channels
-	//cout<<rio.spectrum();
-	cimg_forXY(rio, X, Y)
-		{
-			if (rio(X, Y) <= 1.0) {
-				rio_color(X, Y, 1, 0) = 255.0; //rojo
-				rio_color(X, Y, 1, 1) = 255.0; //verde
-				rio_color(X, Y, 1, 2) = 0.0;
-				//rojo+verde=amarillo
-			} else {//dejo las cosas con el valor que estaban
-				rio_color(X, Y, 1, 0) = rio.get_channel(0)(X, Y);
-				rio_color(X, Y, 1, 1) = rio.get_channel(1)(X, Y);
-				rio_color(X, Y, 1, 2) = rio.get_channel(2)(X, Y);
-			}
-		}
-	rio_color.normalize(0, 255).display(disp3);
-	while (!disp1.is_closed()) {
-	}
+	int umbral = 20;
+	CImg<float> rio_color(rio.width(), rio.height(), 1, 3); // rgb channels
+	rio_color.display(disp3);
+	disp3.set_title("identificacion del rio");
+	while (!disp3.is_closed()) {
+		disp3.wait();
+		if (disp3.is_keyARROWUP())
+			umbral++;
+		else if (disp3.is_keyARROWDOWN())
+			umbral--;
 
+		cimg_forXY(rio, X, Y)
+			{
+				if (rio(X, Y) <= umbral) {
+					rio_color(X, Y, 0, 0) = 255.0; //rojo
+					rio_color(X, Y, 0, 1) = 255.0; //verde
+					rio_color(X, Y, 0, 2) = 0.0;
+					//rojo+verde=amarillo
+				} else {//dejo las cosas con el valor que estaban
+					rio_color(X, Y, 0, 0) = rio(X, Y, 0, 0);
+					rio_color(X, Y, 0, 1) = rio(X, Y, 0, 0);
+					rio_color(X, Y, 0, 2) = rio(X, Y, 0, 0);
+				}
+			}
+		rio_color.display(disp3);
+		disp3.set_title("identificacion del rio");
+		cout<<"Umbral: "<<umbral<<endl;
+	}
 	return 0;
 }
