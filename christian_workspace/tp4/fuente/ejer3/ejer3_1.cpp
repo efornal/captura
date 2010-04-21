a5865ffabc8c22a409f12a948e6228ab672c2fda
christian <c.pfarher@gmail.com>
2010-04-21 01:09:55 -0300

    modifico normalize

--- christian_workspace/tp4/fuente/ejer3/ejer3_1.cpp
+++ christian_workspace/tp4/fuente/ejer3/ejer3_1.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <CImg.h>
 #include <vector>
+#include <string>
 #include "../lib/proc_color.h"
 
 using namespace std;
@@ -15,51 +16,54 @@ using namespace cimg_library;
 
 int main(int argc, char **argv) {
 	CImg<float> imagen(256, 256, 1, 1);
-	CImgDisplay disp1;
+
 	cimg_forXY(imagen, x,y)
 		{
 			imagen(x, y) = x; //genero imagen en tonos de grises
 		}
-	imagen.normalize(0, 255.0);
 	/*
 	 * ../../paletas/bone.pal
-	 ../../paletas/cool.pal
-	 ../../paletas/copper.pal
-	 ../../paletas/gray.pal
-	 ../../paletas/hot.pal
-	 ../../paletas/hsv.pal
-	 ../../paletas/jet.pal
-	 ../../paletas/pink.pal
-	 ../../paletas/ver.txt
+	 * ../../paletas/cool.pal
+	 * ../../paletas/copper.pal
+	 * ../../paletas/gray.pal
+	 * ../../paletas/hot.pal
+	 * ../../paletas/hsv.pal
+	 * ../../paletas/jet.pal
+	 * ../../paletas/pink.pal
 	 * */
-	/*	char *paletas[10] = { "../../paletas/bone.pal", "../../paletas/bone.pal",
-	 "../../paletas/cool.pal", "../../paletas/copper.pal",
-	 "../../paletas/gray.pal", "../../paletas/hot.pal",
-	 "../../paletas/hsv.pal", "../../paletas/jet.pal",
-	 "../../paletas/pink.pal", "../../paletas/ver.txt" };*/
 
-	/*	cout<<paleta[0][0]<<"    "<<paleta[0][1]<<"     "<< paleta[0][2]<<endl;
-	 cout<<paleta[1][0]<<"    "<<paleta[1][1]<<"      "<<paleta[1][2]<<endl*/;
+	vector<vector<float> > paleta; //creo un vector de vectores
+	cargar_paleta(paleta, "../../paletas/bone.pal");
 
-	vector<vector<float> > paleta; //creo un vecotr de vecotres
-	cargar_paleta(paleta, "../../paletas/pink.pal");
-
+	CImgDisplay disp1, disp2, disp3;
 	CImg<float> imagen_con_paleta_aplicada(imagen.width(), imagen.height(), 1,
 			3);
-	CImg<float> la_paleta(256, 1, 1, 3);
+	CImg<float> la_paleta(256, 256, 1, 3);
+	//TODO: hacer funcion cargar paleta que lo aplique sobre unaimagen..
 	cimg_forXY(imagen_con_paleta_aplicada, x, y)
 		{ //es una lut:
-			imagen_con_paleta_aplicada(x, y, 1, 0) = paleta[imagen(x, y)][0]*255; //canal rojo
-			imagen_con_paleta_aplicada(x, y, 1, 1) = paleta[imagen(x, y)][1]*255; //canal verde
-			imagen_con_paleta_aplicada(x, y, 1, 2) = paleta[imagen(x, y)][2]*255; //canal azul
-		/*	la_paleta(x, y, 1, 0) = paleta[x][0]*255;
-			la_paleta(x, y, 1, 1) = paleta[x][1]*255;
-			la_paleta(x, y, 1, 2) = paleta[x][2]*255;*/
+			imagen_con_paleta_aplicada(x, y, 1, 0) = paleta[imagen(x, y)][0]
+					* 255.0; //canal rojo
+			imagen_con_paleta_aplicada(x, y, 1, 1) = paleta[imagen(x, y)][1]
+					* 255.0; //canal verde
+			imagen_con_paleta_aplicada(x, y, 1, 2) = paleta[imagen(x, y)][2]
+					* 255.0; //canal azul
 
+			la_paleta(x, y, 1, 0) = paleta[x][0] * 255;
+			la_paleta(x, y, 1, 1) = paleta[x][1] * 255;
+			la_paleta(x, y, 1, 2) = paleta[x][2] * 255;
 		}
-//	la_paleta.normalize().display();
+	/*FIXME: porque la ultima linea de las imagens se ven mal!?
+	 * porque en la paleta si bien en el archivo tiene valores 0 cuando la despliego no aparece dicho valor?*/
+	la_paleta.print();
 	imagen.display(disp1);
+	disp1.set_title("imagen a la que se le aplica la paleta");
 
+	imagen_con_paleta_aplicada.display(disp2);
+	disp2.set_title("imagen con paleta aplicada");
+
+	la_paleta.display(disp3);
+	disp3.set_title("la paleta");
 
 	while (!disp1.is_closed()) {
 		disp1.wait();
