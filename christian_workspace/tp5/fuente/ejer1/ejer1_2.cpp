26e53f7784dff7d6158536abffb93e0b8f489d4f
christian <c.pfarher@gmail.com>
2010-04-22 16:55:36 -0300

    empezando ejercicio de guia 5

---
+++ christian_workspace/tp5/fuente/ejer1/ejer1_2.cpp
@@ -0,0 +1,23 @@
+/*
+ * ejer1_2.cpp
+ *
+ *  Created on: 22/04/2010
+ *      Author: christian
+ */
+#define cimg_use_fftw3 1
+
+#ifdef cimg_use_fftw3
+extern "C" {
+#include "fftw3.h"}
+#endif
+#include <iostream>
+#include <CImg.h>
+#include "../lib/lib5.h"
+#include "../lib/CPDSI_functions.h"
+
+using namespace std;
+using namespace cimg_library;
+
+int main(int argc, char **argv) {
+	return 0;
+}
