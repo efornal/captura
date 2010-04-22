c798373bad3b63cf2ce13c9c07246b1fec2e075e
christian <c.pfarher@gmail.com>
2010-04-22 00:10:33 -0300

    borro paleta estaba haciendo redundante solo queeda aplicacion en escala de grieses - animado

--- christian_workspace/tp4/fuente/ejer3/ejer3_1.cpp
+++ christian_workspace/tp4/fuente/ejer3/ejer3_1.cpp
@@ -16,8 +16,6 @@ using namespace cimg_library;
 
 int main(int argc, char **argv) {
 	CImg<float> imagen(256, 256, 1, 1);
-	CImg<float> la_paleta(256, 256, 1, 1, 255.0);
-
 	cimg_forXY(imagen, x,y)
 		{
 			imagen(x, y) = x; //genero imagen en tonos de grises
@@ -26,22 +24,33 @@ int main(int argc, char **argv) {
 	vector<vector<float> > paleta; //creo un vector de vectores
 	cargar_paleta(paleta, "../../paletas/bone.pal");
 
-	CImgDisplay disp1, disp2, disp3;
+	CImgDisplay disp1, disp2;
 
-	CImg<float> imagen_con_paleta_aplicada=aplicar_paleta(imagen, 1);
-	la_paleta=aplicar_paleta(la_paleta, 1);
+	CImg<float> imagen_con_paleta_aplicada = aplicar_paleta(imagen, 1);
 
 	imagen.display(disp1);
-	disp1.set_title("imagen a la que se le aplica la paleta");
+	disp1.set_title("USAR TECLAS-imagen a la que se le aplica la paleta");
 
 	imagen_con_paleta_aplicada.display(disp2);
 	disp2.set_title("imagen con paleta aplicada");
 
+	int nro_pal = 1;
 
-	imagen_con_paleta_aplicada.display(disp3);
-	disp3.set_title("paleta aplicada a una imagen blanca");
 	while (!disp1.is_closed()) {
 		disp1.wait();
+		if (disp1.is_keyARROWUP()) {
+			if (nro_pal >= 1 && nro_pal < 8)
+				nro_pal++;
+		} else if (disp1.is_keyARROWDOWN()) {
+			if (nro_pal > 1 && nro_pal <= 8)
+				nro_pal--;
+		}
+		imagen_con_paleta_aplicada = aplicar_paleta(imagen, nro_pal);
+		imagen.display(disp1);
+		disp1.set_title("imagen a la que se le aplica la paleta");
+		cout << "paleta: " << nro_pal << endl;
+		imagen_con_paleta_aplicada.display(disp2);
+		disp2.set_title("imagen con paleta aplicada");
 	}
 	return 0;
 }
