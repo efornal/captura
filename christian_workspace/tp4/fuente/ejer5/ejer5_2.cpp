245f5a72d3470c0fc3f8e7df74b5f8bc34473773
christian <c.pfarher@gmail.com>
2010-04-23 15:36:28 -0300

    empezando ejericcico 5_2

---
+++ christian_workspace/tp4/fuente/ejer5/ejer5_2.cpp
@@ -0,0 +1,62 @@
+/*
+ * ejer5_2.cpp
+ *
+ *  Created on: 23/04/2010
+ *      Author: christian
+ */
+
+#include <iostream>
+#include <math.h>
+#include <CImg.h>
+#include "../../../tp3/fuente/lib/mask.h"
+#include "../lib/proc_color.h"
+
+using namespace std;
+using namespace cimg_library;
+
+int main(int argc, char **argv) {
+	//a--------------------------------------------------------------------------------
+	//generar una mascara de filtrado con suma 1
+	CImg<float> mascara = generar_mascara_PA_suma1<float> (2);
+
+	//b--------------------------------------------------------------------------------
+	// cargar imagen y obtener componentes RGB
+	CImg<float> camino("../../imagenes/camino.tif");
+	CImgDisplay disp1(camino, "camino", 0);
+
+	int dx = camino.width();
+	int dy = camino.height();
+
+	CImg<float> camino_R(dx, dy, 1, 0), camino_G(dx, dy, 1, 0), camino_B(dx,
+			dy, 1, 0);
+	descomponer_rgb<float> (camino, camino_R, camino_G, camino_B);
+	//c--------------------------------------------------------------------------------
+	//procese casa componente por separado y genera una nueva imagen
+	camino_R = camino_R.get_convolve(mascara);
+	camino_G = camino_G.get_convolve(mascara);
+	camino_B = camino_B.get_convolve(mascara);
+
+	CImg<float> imagen_procesada = componer_imagen<float> (camino_R, camino_G,
+			camino_B);
+	CImgDisplay disp2(imagen_procesada,
+			"imagen a la cual se le aplico la mascara por canales", 0);
+
+	//d--------------------------------------------------------------------------------
+	// obtener componentes HSI de la imagen original
+	CImg<float> camino_HSI = camino.get_RGBtoHSI();
+	CImg<float> camino_HSI_H = camino_HSI.get_channel(0);
+	CImg<float> camino_HSI_S = camino_HSI.get_channel(1);
+	CImg<float> camino_HSI_I = camino_HSI.get_channel(2);
+
+	//e--------------------------------------------------------------------------------
+	// filtrar compoennte de intensiddad y visualizar los resultados
+	camino_HSI_I=camino_HSI_I.get_convolve(mascara);
+
+	CImg <float> camino_filtrado_intensidad=(componer_imagen_hsi <float> (camino_HSI_H, camino_HSI_S, camino_HSI_I)).HSItoRGB();
+
+
+	while (!disp1.is_closed()) {
+		disp1.wait();
+	}
+	return 0;
+}
