9a093be244e6d50b0448bdb0625d11c58bbcd252
christian <c.pfarher@gmail.com>
2010-04-23 14:32:03 -0300

    agrego funcion para visualizar canales HSI

--- christian_workspace/tp4/fuente/lib/proc_color.h
+++ christian_workspace/tp4/fuente/lib/proc_color.h
@@ -12,11 +12,22 @@
 using namespace std;
 using namespace cimg_library;
 
+template <class T>
+void visualizar_HSI(CImg <T> &canal_h, CImg <T> &canal_s, CImg <T> &canal_i){
+	/* devuleve por referencia los canales HSI para poder ser visualizadas correctamente*/
+	cimg_forXY(canal_h,x,y)
+		{
+		canal_h(x, y) = canal_h(x, y) / 360.0 * 255.0; //recordar que H esta entre 0 y 360
+		canal_s(x, y) = canal_s(x, y) * 255.0; //recordar que S va de 0 a 1.0
+		canal_i(x, y) = canal_i(x, y) * 255.0; //recordar que I esta entre 0 y 1.0
+		}
+}
+
+
 template<class T>
 CImg<T> componer_imagen(CImg<T> canal_rojo, CImg<T> canal_verde,
 		CImg<T> canal_azul) {
-	CImg<T> imagen(canal_rojo.width(), canal_rojo.height(), canal_rojo.depth(),
-			3);
+	CImg<T> imagen(canal_rojo.width(), canal_rojo.height(), 1, 3);
 	cimg_forXY(imagen, x, y)
 		{
 			imagen(x, y, 0, 0) = canal_rojo(x, y);
@@ -27,12 +38,45 @@ CImg<T> componer_imagen(CImg<T> canal_rojo, CImg<T> canal_verde,
 }
 
 template<class T>
-CImg<T> componer_imagen_hsi(CImg<T> canal_h, CImg<T> canal_s,
-		CImg<T> canal_i) {
+void descomponer_rgb(CImg<T> &imagen_a_descomponer, CImg<T> &canal_R,
+		CImg<T> &canal_G, CImg<T> &canal_B, bool ecualizar_por_separado = false) {
+	/* Descompone una imagen RGB en sus canales
+	 * devuelve los canales R, G y B con sus colores correspondientes por referencia
+	 * ecualizar_por_separado = false implica que no haya ecualizacion por canal
+	 * ecualizar_por_separado = true implica que devuelve por referencia cada canal ecualizado
+	 */
+	cimg_forXY(imagen_a_descomponer,x,y)
+		{
+			canal_R(x, y, 0, 0) = imagen_a_descomponer(x, y, 0, 0);//canal rojo
+			canal_R(x, y, 0, 1) = 0; //POSTA: guarda porque la cimg_forXY te hace para todos los C si no le especificas nada!!
+			canal_R(x, y, 0, 2) = 0;
+			canal_G(x, y, 0, 1) = imagen_a_descomponer(x, y, 0, 1);//canal rojo
+			canal_B(x, y, 0, 2) = imagen_a_descomponer(x, y, 0, 2);//canal rojo
+		}
+	if (ecualizar_por_separado) { //ecualizacion de cada canal por serparado
+		canal_R.equalize(0, 255);
+		canal_G.equalize(0, 255);
+		canal_B.equalize(0, 255);
+		imagen_a_descomponer = componer_imagen<T> (canal_R, canal_G, canal_B);
+		cout << endl << "Se ecualizaron los canales por serparado" << endl;
+	}
+}
+
+template<class T>
+CImg<T> ecualizar_por_separado(CImg<T> imagen) {
+	/* Ecualiza cada canal por serparado y devuelve una imagen con los 3 canales ecualizados
+	 * */
+	CImg<T> imagen_devolver(imagen.width(), imagen.height(), 1, 3);
+	return componer_imagen<T> (imagen.get_channel(0).equalize(255),
+			imagen.get_channel(1).equalize(255),
+			imagen.get_channel(2).equalize(255));
+}
+
+template<class T>
+CImg<T> componer_imagen_hsi(CImg<T> canal_h, CImg<T> canal_s, CImg<T> canal_i) {
 	/*Tener en cuenta que los canales que deben pasarseles a esta funcion son tal cula fueron devueltos por la fucnion
 	 * get_RGB_TO_HSI()*/
-	CImg<T> imagen(canal_h.width(), canal_h.height(), 1,
-			3);
+	CImg<T> imagen(canal_h.width(), canal_h.height(), 1, 3);
 	cimg_forXY(imagen, x, y)
 		{
 			imagen(x, y, 0, 0) = canal_h(x, y, 0, 0);
@@ -49,12 +93,10 @@ CImg<T> invertir_HSI(CImg<T> imagen_H) {
 	 * */
 	cimg_forXY(imagen_H, x, y)
 		{
-		imagen_H(x, y, 0, 0) = imagen_H(imagen_H.height()-1-x, y, 0, 0); //reflexion horizontal
+			imagen_H(x, y, 0, 0) = imagen_H(imagen_H.height() - 1 - x, y, 0, 0); //reflexion horizontal
 		}
 	return imagen_H;
 }
-
-
 
 CImg<float> aplicar_paleta(CImg<float> imagen, int numero_paleta = 1) {
 	/* carga una paleta con nombre_paleta la aplica a la imagen pasada como parametro y
