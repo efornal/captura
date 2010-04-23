ed87e2c135fcf8009b29f61f172b7c101c299777
christian <c.pfarher@gmail.com>
2010-04-23 14:32:53 -0300

    arreglo ejercicios

--- christian_workspace/tp4/fuente/ejer5/ejer5_1_a.cpp
+++ christian_workspace/tp4/fuente/ejer5/ejer5_1_a.cpp
@@ -14,25 +14,18 @@ using namespace cimg_library;
 int main(int argc, char **argv) {
 	CImg<float> imagen_oscura("../../imagenes/chairs_oscura.jpg"); //cargo la imagen
 
-	imagen_oscura.resize(200,200);
-	int dx=imagen_oscura.width();
-	int dy=imagen_oscura.height();
+	imagen_oscura.resize(200, 200);
+	int dx = imagen_oscura.width();
+	int dy = imagen_oscura.height();
 
-	CImg<float> imagen_oscura_R(dx,dy,1,3), imagen_oscura_G(dx,dy,1,3), imagen_oscura_B(dx,dy,1,3);
-	cimg_forXY(imagen_oscura_R,x,y){
-		imagen_oscura_R(x,y,0,0)= imagen_oscura(x,y,0,0);//canal rojo
-		imagen_oscura_R(x,y,0,1)= 0; //POSTA: guarda porque la cimg_forXY te hace para todos los C si no le especificas nada!!
-		imagen_oscura_R(x,y,0,2)= 0;
-		imagen_oscura_G(x,y,0,1)= imagen_oscura(x,y,0,1);//canal rojo
-		imagen_oscura_B(x,y,0,2)= imagen_oscura(x,y,0,2);//canal rojo
-	}
+	CImg<float> imagen_oscura_R(dx, dy, 1, 3), imagen_oscura_G(dx, dy, 1, 3),
+			imagen_oscura_B(dx, dy, 1, 3);
+	descomponer_rgb(imagen_oscura, imagen_oscura_R, imagen_oscura_G,
+			imagen_oscura_B); //descompongo la imagen en 3 canales
 
-	CImgList <float> lista (imagen_oscura, imagen_oscura_R, imagen_oscura_G, imagen_oscura_B);
-	CImgDisplay disp1(lista, "imagen oscura, canal R, canal G, canal B",0);
-
-
-
-
+	CImgList<float> lista(imagen_oscura, imagen_oscura_R, imagen_oscura_G,
+			imagen_oscura_B);
+	CImgDisplay disp1(lista, "imagen oscura, canal R, canal G, canal B", 0);
 
 
 	while (!disp1.is_closed()) {
--- christian_workspace/tp4/fuente/ejer5/ejer5_1_b.cpp
+++ christian_workspace/tp4/fuente/ejer5/ejer5_1_b.cpp
@@ -1,5 +1,5 @@
 /*
- * ejer5_1_a.cpp
+ * ejer5_1_b.cpp
  *
  *  Created on: 22/04/2010
  *      Author: christian
@@ -7,25 +7,41 @@
 
 #include <iostream>
 #include <CImg.h>
-
+#include "../lib/proc_color.h"
 using namespace std;
 using namespace cimg_library;
 
 int main(int argc, char **argv) {
+	CImg<float> imagen_oscura("../../imagenes/chairs_oscura.jpg"); //cargo la imagen
 
-	//parte b - ecualizar cada componente por separado
-		CImg<float> imagen_oscura_R_equalizada(imagen_oscura_R),
-				imagen_oscura_G_equalizada(imagen_oscura_G),
-				imagen_oscura_B_equalizada(imagen_oscura_B);
+	imagen_oscura.resize(300, 300);
+	int dx = imagen_oscura.width();
+	int dy = imagen_oscura.height();
 
-		imagen_oscura_R_equalizada.equalize(255);
-		cout << "spectrum: " << imagen_oscura_R_equalizada.spectrum();
-		imagen_oscura_G_equalizada.equalize(255);
-		imagen_oscura_B_equalizada.equalize(255);
-		CImg<float> imagen_oscura_ecualizada_intependiente = componer_imagen<CImg<
-				float> > (imagen_oscura_R_equalizada, imagen_oscura_R_equalizada,
-				imagen_oscura_R_equalizada);
-		CImg<float> imagen_oscura_ecualizada_global = imagen_oscura.get_equalize(
-				255);
-return 0;
+	CImg<float> imagen_oscura_R(dx, dy, 1, 3), imagen_oscura_G(dx, dy, 1, 3),
+			imagen_oscura_B(dx, dy, 1, 3);
+	descomponer_rgb(imagen_oscura, imagen_oscura_R, imagen_oscura_G,
+			imagen_oscura_B); //descompongo la imagen en 3 canales
+
+	CImgList<float> lista(imagen_oscura, imagen_oscura_R, imagen_oscura_G,
+			imagen_oscura_B);
+	CImgDisplay disp1(lista, "imagen oscura, canal R, canal G, canal B", 0);
+
+	//------------------------------------------------------------
+	//ecualizar cada componentee por serparado y mostrar:
+	CImg<float> imagen_oscura_ecualizada(imagen_oscura), imagen_oscura_R_eq(dx,
+			dy, 1, 3), imagen_oscura_G_eq(dx, dy, 1, 3), imagen_oscura_B_eq(dx,
+			dy, 1, 3);
+	descomponer_rgb<float> (imagen_oscura_ecualizada, imagen_oscura_R_eq,
+			imagen_oscura_G_eq, imagen_oscura_B_eq, true);
+	CImgList<float> lista1(imagen_oscura_ecualizada, imagen_oscura_R_eq,
+			imagen_oscura_G_eq, imagen_oscura_B_eq);
+	CImgDisplay
+			disp2(
+					lista,
+					"imagen oscura ecualizada, canal R ecualizado, canal G ecualizado, canal B ecualizado",
+					0);
+	while (!disp1.is_closed()) {
+	};
+	return 0;
 }
