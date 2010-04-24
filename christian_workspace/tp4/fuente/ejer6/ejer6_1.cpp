4a40fc24ce5f316bd722913a8436a5103385d2ea
christian <c.pfarher@gmail.com>
2010-04-24 18:23:38 -0300

    ejercico 6_1 andando

--- christian_workspace/tp4/fuente/ejer6/ejer6_1.cpp
+++ christian_workspace/tp4/fuente/ejer6/ejer6_1.cpp
@@ -22,24 +22,6 @@ int main(int argc, char **argv) {
 	 1 - Tomas el color de un pixel (R,G,B) el cual queres segmentar.
 	 2 - Pensá en (R,G,B) como las coordenadas (x,y,z) de un punto dentro del cubo unitario. Si segmentas estrictamente ese color entonces preguntas si cada pixel es igual al      color a segmentar o no. Para darle más flexibilidad hay q dejar pasar los colores que son "parecidos" a (R,G,B), es decir a los puntos que están cerca a (R,G,B) en el         cubo unitario; es decir, los puntos que están dentro de la esfera con centro (R,G,B) y un radio de tolerancia.
 	 3 - Por cada pixel leído con componentes (r,g,b) calculas la distancia a (R,G,B)
-	 4 - Ahí va el cógido:
-
-	 CImg<tipo> segmentaRGB(CImg<tipo>img, float radio_tol, float R, float G, float B,float *color){
-	 radio_tol*=radio_tol;
-	 float distancia2,r,g,b;
-	 R/=256.f, G/=256.f, B/=256.f; // centro de la esfera
-	 CImg<tipo> img_seg=img.normalize(0,1);
-	 cimg_forXY(img,x,y){
-	 r=img(x,y,0,0), g=img(x,y,0,1), b=img(x,y,0,2);
-	 distancia2=(r-R)*(r-R)+(g-G)*(g-G)+(b-B)*(b-B);
-	 if(distancia2>radio_tol){
-	 img_seg(x,y,0,0)=color[0];
-	 img_seg(x,y,0,1)=color[1];
-	 img_seg(x,y,0,2)=color[2];
-	 }
-	 }
-	 return img_seg;
-	 }
 
 	 En HSI: en vez de unn cubo unitario a tenes un plano, y en vez de una esfera tenes un rectángulo.
 	 Si alguien sabe como corno usar el histograma para q ayud en estos casos q me diga!
@@ -49,12 +31,36 @@ int main(int argc, char **argv) {
 	//a---------------------------------------------------------------------------------------
 	//cargue la imagen futbol.jpg
 	CImg<float> imagen("../../imagenes/futbol.jpg");
+	//imagen.display();
 	CImgDisplay disp1(imagen, "imagen de futbol", 0);
+	CImgDisplay disp2;
 	//b---------------------------------------------------------------------------------------
 	// tome una muestra representativa del color a segmentar y calcule el centro de la esfera
 	// (valor medio de cada compoennete)
+	int posx = 149, posy = 135;
+	float color_original[] = { imagen(posx, posy, 0, 0), imagen(posx, posy, 0,
+			1), imagen(posx, posy, 0, 2) };
+
+	float tol = 0.01;
+	segmentaRGB<float> (imagen, tol, imagen(posx, posy, 0, 0), imagen(posx,
+			posy, 0, 1), imagen(posx, posy, 0, 2), color_original).display(
+			disp2);
+	disp1.set_title("use la ruedita del raton y el click");
 	while (!disp1.is_closed()) {
 		disp1.wait();
+		if (disp1.button()) {
+			posx = disp1.mouse_x();
+			posy = disp1.mouse_y();
+			color_original[0]=imagen(posx, posy, 0, 0);
+			color_original[1]=imagen(posx, posy, 0, 1);
+			color_original[2]=imagen(posx, posy, 0, 2);
+
+		}
+		segmentaRGB<float> (imagen, tol + (disp1.wheel() * tol), imagen(posx,
+				posy, 0, 0), imagen(posx, posy, 0, 1), imagen(posx),
+				color_original).display(disp2);
+		disp2.set_title("imagen segmentada");
+		cout << "tolerancia: " << tol + (disp1.wheel() * tol) << endl;
 	}
 
 	return 0;
