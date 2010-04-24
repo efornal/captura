5f025a17b767b81cc2ffff7bb16db838ec586c46
christian <c.pfarher@gmail.com>
2010-04-24 12:50:28 -0300

    animaciones con el mouse

--- christian_workspace/tp5/fuente/ejer1/ejer1_2.cpp
+++ christian_workspace/tp5/fuente/ejer1/ejer1_2.cpp
@@ -22,16 +22,21 @@ using namespace cimg_library;
 
 int main(int argc, char **argv) {
 	CImg<double> imagen0(300, 300, 1, 1);
-	CImg<double> imagen1(300, 300, 1, 1);
 	CImg<unsigned char> mod_0(300, 300, 1, 1);
+
+	CImg<double> imagen1(300, 300, 1, 1);
 	CImg<unsigned char> mod_1(300, 300, 1, 1);
+
 	CImg<double> imagen2(300, 300, 1, 1);
 	CImg<unsigned char> mod_2(300, 300, 1, 1);
+
 	CImg<double> imagen3(300, 300, 1, 1);
 	CImg<unsigned char> mod_3(300, 300, 1, 1);
+
 	float radio = 50.0;
-	int posx=imagen0.width()/2;
-	int posy=imagen0.height()/2;
+	int posx = imagen0.width() / 2;
+	int posy = imagen0.height() / 2;
+
 	circulo_centrado(posx, posy, imagen0, radio);
 	cuadrado_centrado(imagen1, 30, 30);
 	linea_vertical(imagen2, imagen2.width() / 2);
@@ -53,29 +58,11 @@ int main(int argc, char **argv) {
 	CImgDisplay disp2(lista1, "imagen y su transformada");
 	CImgDisplay disp3(lista2, "imagen y su transformada");
 	CImgDisplay disp4(lista3, "imagen y su transformada");
-	/*CImg<unsigned char> modulo;
-	 magn_tdf(imagen, modulo, 1); //transformada con la funcion de la catedra
-	 CImgDisplay disp2(modulo, "modulo de la imagen", 0);*/
-	while (!disp1.is_closed()) {
 
-		if (disp1.is_keyPAGEUP()) {//crecer radio
-			radio++;
-			cout<<"radio: "<<radio<<endl;
-		} else if (disp1.is_keyPAGEDOWN()) {
-			//decrece radio
-			radio-=1.0;
-			cout<<"radio: "<<radio<<endl;
-		} else if (disp1.is_keyARROWRIGHT()){
-			posx++;
-		} else if (disp1.is_keyARROWLEFT()){
-			posx--;
-		} else if (disp1.is_keyARROWUP()){
-			posy--;
-		} else if (disp1.is_keyARROWDOWN()){
-			posy++;
-		}
+	while (!disp1.is_closed()) {
 		//mostrar
-		circulo_centrado(posx, posy, imagen0, radio);
+		circulo_centrado(disp1.mouse_x(), disp1.mouse_y(), imagen0, radio
+				+ disp1.wheel());
 		magn_tdf(imagen0, mod_0, 1);
 		imagen0.display(disp1);
 		mod_0.display(disp1_1);
