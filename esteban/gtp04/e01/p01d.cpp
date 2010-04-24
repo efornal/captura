012dcadbd8cd5435b128ae2dc81d0f3168ccadfb
esteban <efornal@gmail.com>
2010-04-24 11:04:30 -0300

    brillo maximo a la imagen en hsi

---
+++ esteban/gtp04/e01/p01d.cpp
@@ -0,0 +1,34 @@
+#include <CImg.h>
+#include <iostream>
+using namespace cimg_library;
+using namespace std;
+
+int main( int argc, char **argv ) {
+    const char *filename = cimg_option( "-f", "../../imagenes/patron.tif", 
+                                        "ruta archivo imagen" );
+    CImg<double> img ( filename );
+    //CImg<double> img ( 300, 300,1, 3, 100); usando img cte
+    CImg<double> img_hsi, img_rgb;
+    CImgDisplay disp, disp2, disp3, disp4;
+
+    img.display(disp); 
+    img.print("RGB");
+
+    disp.set_title("img original");
+    img_hsi = img.get_RGBtoHSI();
+    img_hsi.print("HSI");
+
+    cimg_forXY(img_hsi,x,y){
+        img_hsi(x,y,0,2) =  1;
+    }
+    img_hsi.get_channel(2).display("i = 1 CTE");
+
+    img_rgb = img_hsi.get_HSItoRGB();
+    img_rgb.display("i = 1 CTE(max brillo)");
+
+
+    while ( (!disp.is_closed() &&  !disp.is_keyQ()) ) {
+        disp.wait();
+    }
+    return 0;
+}
