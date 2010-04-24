a647fcbcb6af1a71e6a26d5c5022591fa678a4cd
esteban <efornal@gmail.com>
2010-04-24 10:29:51 -0300

    organizado puntos del ejercicio segun la guia

--- esteban/gtp04/e01/p01a.cpp
+++ esteban/gtp04/e01/p01a.cpp
@@ -1,15 +1,16 @@
 /**
- * FIXME confirmar:
- * Dado que rotar los valores de h mediante una suma o resta
- * de angulo, no se puede llegar cd RGB a BGR, no se puede hacer
- * la transformacion mediante suma o resta de angulo (ademas 
- * habria que tener en cuenta no pasarse de 240 grados)
- * a) partiendo de R-y-G-c-B(0-240º) sumando  180 obtenemos BRG
- * b) partiendo de R-y-G-c-B(0-240º) restando 180 obtenemos GBR
- * en ambos casos se considera que si pasa de 240 comienza en cero!
- * la opcion que queda es invertir valores el R<->B
+ * HSI - variacion componente h [0-360] pero aca en [0-240]
+ * esta imagen varia en sus valores de 0-240(angulo) RGB (ver triang colores)
+ * por tanto se visualiza como un degrade de 0 a 240
+ *
+ * HSI - variacion componente s [0-1]
+ * esta imagen es de un color omogeneo (CTE) debido que para todos
+ * los colores del degrade anterior, tiene la misma saturacion
+ *
+ * HSI - variacion componente i [0-1]
+ * la intencidad varia segun la mezcla de las 3 componenetes(en este caso)
+ * 
 */
-
 #include <CImg.h>
 #include <iostream>
 using namespace cimg_library;
@@ -21,7 +22,10 @@ int main( int argc, char **argv ) {
 
     const int h = cimg_option( "-h", 0,"valor de h en grados" );
 
-    CImg<double> img ( filename ),img_hsi, img_rgb, aux;
+    CImg<double> img ( filename ),img_hsi, img_rgb;
+    CImg<double> img_h(img.width(),img.height(),1,3,0);
+    CImg<double> img_s(img.width(),img.height(),1,3,0);
+    CImg<double> img_i(img.width(),img.height(),1,3,0);
     CImgDisplay disp, disp2, disp3, disp4;
 
     img.display(disp); 
@@ -29,20 +33,22 @@ int main( int argc, char **argv ) {
 
     disp.set_title("img original");
     img_hsi = img.get_RGBtoHSI();
-    aux = img_hsi;
     img_hsi.print("HSI");
 
-    //inversion de valores
     cimg_forXY(img_hsi,x,y){
-        aux(x,y,0,0) =  img_hsi( img_hsi.width()-x-1, y, 0, 0);
+        img_h(x,y,0,0) =  img_hsi(x,y,0,0);
+        img_s(x,y,0,1) =  img_hsi(x,y,0,1);
+        img_i(x,y,0,2) =  img_hsi(x,y,0,2);
     }
     
-    img_rgb = aux.get_HSItoRGB();
-    img_rgb.display(disp4);
-    disp4.set_title("img h invertida");
+    img_rgb = img_hsi.get_HSItoRGB();
+    img_h.normalize(0,255).display("HSI - variacion componente h");
+    img_s.normalize(0,255).display("HSI - variacion componente s");
+    img_i.normalize(0,255).display("HSI - variacion componente i");
 
     while ( (!disp.is_closed() &&  !disp.is_keyQ()) ) {
         disp.wait();
     }
+
     return 0;
 }
