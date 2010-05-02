b10d964e68545327ef315afd2f183487c4f90a9a
christian <c.pfarher@gmail.com>
2010-05-02 14:53:44 -0300

    corrigo llamada a funcion

--- christian_workspace/tp2/fuente/ejer2/ejer2_1.cpp
+++ christian_workspace/tp2/fuente/ejer2/ejer2_1.cpp
@@ -15,7 +15,7 @@ using namespace std;
 using namespace cimg_library;
 
 int main() {
-	obtener_grafica_mapeo_potencia(2).display("Grafica Potencia");
+	obtener_grafica_mapeo_potencia<CImg <int> >(2).display("Grafica Potencia");
 	obtener_grafica_mapeo_logaritmo(15).display("Grafica Logaritmo");
 	return 0;
 }
