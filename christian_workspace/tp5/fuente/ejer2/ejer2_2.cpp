6ff84780b13a386578161798fee8337e91ef1f37
christian <c.pfarher@gmail.com>
2010-05-02 14:58:04 -0300

    tratando de hacer algo

--- christian_workspace/tp5/fuente/ejer2/ejer2_2.cpp
+++ christian_workspace/tp5/fuente/ejer2/ejer2_2.cpp
@@ -22,11 +22,21 @@ using namespace cimg_library;
 
 int main(int argc, char **argv) {
 	CImg<double> img("../../imagenes/avion.jpg"); //imagen original
-	CImg<double> img_aeropuerto ("../../imagenes/aeropuerto.jpg");
-	CImgList <double> lista_imagenes(img, img_aeropuerto);
+	CImg<double> img_aeropuerto("../../imagenes/aeropuerto.jpg");
+	CImgList<double> lista_imagenes(img, img_aeropuerto);
 	CImgDisplay displis1(lista_imagenes);
 	displis1.set_title("Imagenes originales");
-//TODO: hacerlo...
+	//TODO: hacerlo.....
+
+	CImg<double> img_fase = get_fase<double> (img); //obtengo fase de la fft
+	CImg<double> img_magnitud = get_magnitud<double> (img_aeropuerto);
+	CImgList <double> lista1(img_fase.width(), img_fase.height(), 1, 2);
+	cimg_forXY(img_fase, x, y)
+		{
+			lista1[0](x, y) = img_magnitud(x, y);
+			lista1[1](x, y) = img_fase(x, y);
+		}
+	lista1.get_FFT(true).display();
 
 	while (!displis1.is_closed()) {
 		displis1.wait();
