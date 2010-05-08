8f76012ffa239804ca011c8d3de7530ced774b1b
christian <c.pfarher@gmail.com>
2010-05-08 10:16:42 -0300

    D(u,v) era la distancia,,, corregido,, andando.. esta bien? comparar ocnchaco

--- christian_workspace/tp5/fuente/ejer3/ejer3_3.cpp
+++ christian_workspace/tp5/fuente/ejer3/ejer3_3.cpp
@@ -30,35 +30,33 @@ int main(int argc, char **argv) {
 	CImg<unsigned char> img(filename); //imagen original
 	img.normalize(0, 255);
 	CImgDisplay disporiginal(img, "imagen original");
-	int frec_corte = 150;
+	int frec_corte = 50;
 	int orden = 1;
 	CImg<float> H(img.width(), img.height(), 1, 1);
 
 	CImgDisplay disp1, disp2;
-
-	CImg<float> filtrada = aplicar_Butter_PB<float> (img, frec_corte, orden);
+	CImg<float> filtrada = aplicar_Butter_PB<float> (img, H, frec_corte, orden);
 	filtrada.display(disp1);
 	disp1.set_title("imagen filtrada");
-	/*	H.display(disp2);
-//TODO: esto no anda...
-	 disp2.set_title("Filtro que se aplica");*/
+	H.display(disp2);
+	disp2.set_title("Filtro que se aplica");
 	while (!disp1.is_closed()) {
+		disp1.wait();
 		if (disp1.is_keyARROWRIGHT()) {
-			frec_corte++;
+			frec_corte+=2;
 		} else if (disp1.is_keyARROWLEFT()) {
-			frec_corte--;
+			frec_corte-=2;
 		} else if (disp1.is_keyARROWUP()) {
 			orden++;
 		} else if (disp1.is_keyARROWDOWN()) {
 			orden--;
 		}
-		filtrada = aplicar_Butter_PB<float> (img, frec_corte, orden);
-		filtrada.log().display(disp1);
+		filtrada = aplicar_Butter_PB<float> (img, H, frec_corte, orden);
+		filtrada.display(disp1);
 		disp1.set_title("imagen filtrada");
 
-		/*		H.display(disp1);
-		 disp1.set_title("Filtro que se aplica");*/
-
+		H.display(disp2);
+		disp2.set_title("Filtro que se aplica");
 		cout << "Frec. corte: " << frec_corte << "     Orden: " << orden
 				<< endl;
 	}
--- christian_workspace/tp5/fuente/lib5/filtros.h
+++ christian_workspace/tp5/fuente/lib5/filtros.h
@@ -38,17 +38,22 @@ void Butterworth(CImg <T> &imagen, int frec_corte=10, int orden=1){
 }*/
 //TODO: esto todavia no funca...
 template<class T>
-CImg<T> aplicar_Butter_PB(CImg<T> imagen, int frec_corte=10, int orden=1) {
+CImg<T> aplicar_Butter_PB(CImg<T> imagen, CImg <T> &H, int frec_corte=10, int orden=1) {
 	/*aplica un filtro pasa Bajos Butterwortch con frecuencia de corte: frec_corte y orden = orden.
 	 * Por defecto: frec_corte=10 y orden=1
 	 * Devuelve la imagen filtrada para ser mostrada.
+	 * devuelve por referencia H para poder plotear el filtro...
 	 */
-	CImg<T> H(imagen.width(), imagen.height(), 1, 1);
+	float distancia;
+	float mediox=imagen.width()/2.0;
+	float medioy=imagen.height()/2.0;
+	//CImg<T> H(imagen.width(), imagen.height(), 1, 1);
 	cimg_forXY(H,x,y){
-		H(x,y)=1.0/(1.0+pow((H(x,y)/frec_corte),2.0*orden));
+		distancia = sqrt ( pow(x-mediox,2.0) + pow(y-medioy,2.0) );
+		H(x,y)=1.0/(1.0+pow(distancia/abs(frec_corte), 2.0*abs(orden)));
 	}
-	H.normalize(0.0, 1.0);
-	H.shift(H.width() / 2, H.height() / 2, 0, 0, 2); //centro la func de transferencia
+	//H.normalize(0.0, 1.0);
+	//H.shift(H.width() / 2, H.height() / 2, 0, 0, 2); //centro la func de transferencia
 	CImgList<T> F = imagen.get_FFT();//obtengo la transformada
 	CImg<T> F_real = F[0]; //parte real
 	CImg<T> F_imag = F[1]; //parte imaginaria
