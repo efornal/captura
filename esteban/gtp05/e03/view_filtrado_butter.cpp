f9651dae1ff1f889f34e70a5205ce3d32de4d694
esteban <efornal@gmail.com>
2010-05-07 23:17:14 -0300

    combinacion de teclas para modificar el orden del filtro butter

--- esteban/gtp05/e03/view_filtrado_butter.cpp
+++ esteban/gtp05/e03/view_filtrado_butter.cpp
@@ -11,14 +11,14 @@ using namespace std;
 int main( int argc, char **argv ) {
     const char *filename = cimg_option( "-f", "../../imagenes/huang2.jpg", 
                                         "ruta archivo imagen" );
-    int wc = cimg_option( "-wc", 100, "frecuencia de corte" );
-    int dw = cimg_option( "-dw", 1, "delta de frecuencia" );
+    int wc    = cimg_option( "-wc", 100, "frecuencia de corte" );
+    int dw    = cimg_option( "-dw", 1, "delta de frecuencia" );
+    int orden = cimg_option( "-orden", 1, "orden del filtro" );
 
     CImgDisplay disp, disp2, disp3, disp4, disp5, disp6;
 
-
     CHImg<double> img ( filename );
-    CHImg<double> filtro = filtro::pb_butter( img, wc );
+    CHImg<double> filtro = filtro::pb_butter( img, wc, orden );
 
     img.normalize(0,255).display(disp);
     img.get_fft_modulo_log().normalize(0,255).display(disp2);
@@ -43,7 +43,7 @@ int main( int argc, char **argv ) {
 
         if ( disp.is_keyARROWDOWN () ){
             wc -= dw;
-            filtrada = img.get_filtrada( filtro::pb_butter( img, wc ) );
+            filtrada = img.get_filtrada( filtro::pb_butter( img, wc, orden ) );
             filtrada.normalize(0,255).display(disp4);
             filtrada.get_fft_modulo_log().normalize(0,255).display(disp5);
             filtrada.get_fft_fase().normalize(0,255).display(disp6);
@@ -51,20 +51,21 @@ int main( int argc, char **argv ) {
         }
         if ( disp.is_keyARROWUP () ){
             wc += dw;
-            filtrada = img.get_filtrada( filtro::pb_butter( img, wc ) );
+            filtrada = img.get_filtrada( filtro::pb_butter( img, wc, orden ) );
             filtrada.normalize(0,255).display(disp4);
             filtrada.get_fft_modulo_log().normalize(0,255).display(disp5);
             filtrada.get_fft_fase().normalize(0,255).display(disp6);
             printf("wc: %d\n", wc);
         }
-        if ( disp.is_keyARROWLEFT () ) {
-            dw--;
-            printf("dw: %d\n", dw);
-        }
-        if ( disp.is_keyARROWRIGHT () ) {
-            dw++;
-            printf("dw: %d\n", dw);
-        }
+
+        if ( disp.is_keyARROWLEFT  () ) { orden--; printf("orden: %d\n", orden); }
+        if ( disp.is_keyARROWRIGHT () ) { orden++; printf("orden: %d\n", orden); }
+
+        if ( disp.is_keySHIFTLEFT() && 
+             disp.is_keyARROWUP()   ) { dw++; printf("dw: %d\n", dw); }
+        if ( disp.is_keySHIFTLEFT() && 
+             disp.is_keyARROWDOWN() ) { dw--; printf("dw: %d\n", dw); }
+
     }
     return 0;
 }
