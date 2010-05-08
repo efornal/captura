8cb90d66b193cf64c982dd5f7762dfcf01fa719d
esteban <efornal@gmail.com>
2010-05-08 13:46:57 -0300

    view para filtrado mediante butter pa

---
+++ esteban/gtp05/e04/view_filtrado_butter.cpp
@@ -0,0 +1,72 @@
+/**
+ * ver: /view_filtrado_butter -f ../../imagenes/del-libro/test_pattern_blurring_orig.tif
+*/
+#include <CHImg.h>
+#include <iostream>
+#include <filtro.h>
+
+using namespace cimg_library;
+using namespace std;
+
+int main( int argc, char **argv ) {
+    const char *filename = cimg_option( "-f", "../../imagenes/huang2.jpg", 
+                                        "ruta archivo imagen" );
+    int wc    = cimg_option( "-wc", 100, "frecuencia de corte" );
+    int dw    = cimg_option( "-dw", 1, "delta de frecuencia" );
+    int orden = cimg_option( "-orden", 1, "orden del filtro" );
+
+    CImgDisplay disp, disp2, disp3, disp4, disp5, disp6;
+
+    CHImg<double> img ( filename );
+    CHImg<double> filtro = filtro::pa_butter( img, wc, orden );
+
+    img.normalize(0,255).display(disp);
+    img.get_fft_modulo_log().normalize(0,255).display(disp2);
+    img.get_fft_fase().normalize(0,255).display(disp3);
+
+    disp.set_title("original");
+    disp2.set_title("original: modulo log");
+    disp3.set_title("original: fase");
+
+    CHImg<double> filtrada = img.get_filtrada( filtro );
+
+    filtrada.normalize(0,255).display(disp4);
+    filtrada.get_fft_modulo_log().normalize(0,255).display(disp5);
+    filtrada.get_fft_fase().normalize(0,255).display(disp6);
+
+    disp4.set_title("filtrada");
+    disp5.set_title("filtrada: modulo log");
+    disp6.set_title("filtrada: fase");
+
+    while ( (!disp.is_closed() &&  !disp.is_keyQ()) ) {
+        disp.wait();
+
+        if ( disp.is_keyARROWDOWN () ){
+            wc -= dw;
+            printf("wc: %d\n", wc);
+        }
+        if ( disp.is_keyARROWUP () ){
+            wc += dw;
+            printf("wc: %d\n", wc);
+        }
+
+        if ( disp.is_keyARROWLEFT  () ) { 
+            orden--; 
+            printf("orden: %d\n", orden); }
+        if ( disp.is_keyARROWRIGHT () ) { 
+            orden++; 
+            printf("orden: %d\n", orden); }
+
+        if ( disp.is_keyPAGEUP()   ) { dw--; printf("dw: %d\n", dw); }
+        if ( disp.is_keyPAGEDOWN() ) { dw++; printf("dw: %d\n", dw); }
+
+        if ( disp.is_event () ){ 
+            filtrada = img.get_filtrada( filtro::pa_butter( img, wc, orden ) );
+            filtrada.normalize(0,255).display(disp4);
+            filtrada.get_fft_modulo_log().normalize(0,255).display(disp5);
+            filtrada.get_fft_fase().normalize(0,255).display(disp6);
+        }
+
+    }
+    return 0;
+}
