9abfb5d8e0676aabe0da05d7dd55fb566502bc99
esteban <efornal@gmail.com>
2010-05-07 21:47:35 -0300

    corregido y agregado vista filtro 3D

--- esteban/gtp05/e03/p01.m
+++ esteban/gtp05/e03/p01.m
@@ -1,4 +1,10 @@
-h = zeros(25,25);
-h(11:15,11:15) = 1;
-H=fft(h);
-mesh([1:25],[1:25],fftshift(abs(H)))
+H = zeros(25,25);
+H(11:15,11:15) = 1;
+h=ifft2(H);
+
+mesh([1:25],[1:25],H)
+title("frecuencia H")
+
+figure
+mesh([1:25],[1:25],fftshift(abs(h)))
+title("tiempo h")
---
+++ esteban/gtp05/e03/p03.m
@@ -0,0 +1,17 @@
+wc = 25
+H  = zeros(50,50);
+
+for i=1:50
+  for j=1:50
+    d = sqrt( (i-25)^2 + (j-25)^2 );
+    H(i,j) = 1 / (1+(d/wc));
+  endfor
+endfor
+
+figure
+mesh([1:50],[1:50],H)
+title("frec H")
+
+figure
+mesh([1:50],[1:50],fftshift(abs(ifft2(H))))
+title ("tiempo h")
