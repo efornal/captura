aa86c93e53c9665239d1b36eac759885c12a0968
christian <c.pfarher@gmail.com>
2010-05-15 18:08:00 -0300

    agrego archivos binarios y borro otros

---
+++ christian_workspace/tp5/fuente/ejer8/ejer8_1.cpp
@@ -0,0 +1,30 @@
+/*
+ * ejer9_1c.cpp
+ *
+ *  Created on: 15/05/2010
+ *      Author: christian
+ */
+#define cimg_use_fftw3 1
+
+#ifdef cimg_use_fftw3
+extern "C" {
+#include "fftw3.h"
+}
+#endif
+
+#include <iostream>
+#include <CImg.h>
+
+#include "../lib5/lib5.h"
+#include "../lib5/filtros.h"
+
+using namespace std;
+using namespace cimg_library;
+
+int main(int argc, char **argv) {
+	const char
+			*filename =
+					cimg_option( "-f", "../../imagenes/casilla.tif", "ruta archivo imagen" );
+
+	return 0;
+}
