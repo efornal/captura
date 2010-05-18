8efa57701ebf5c4452b1f7c051d151380d6e79c2
christian <c.pfarher@gmail.com>
2010-05-18 09:50:46 -0300

    vuelvo atras... dejo para ocrregir mas adelante

--- christian_workspace/tp5/fuente/ejer3/ejer3_1.cpp
+++ christian_workspace/tp5/fuente/ejer3/ejer3_1.cpp
@@ -27,27 +27,26 @@ int main(int argc, char **argv) {
 			*filename =
 					cimg_option( "-f", "../../imagenes/huang2.jpg", "ruta archivo imagen" );
 	CImg<double> img(filename); //imagen original
-	CImg<double> img1(21, 21, 1, 1, 0), img2(100, 100, 1, 1, 0), img3(200, 200,
+	//fixme: ver si es convolucion o multiplicacion o que !!!! el filtro que diseno es frecuencia ya no? preguntar!!!
+	CImg<double> img1(25, 25, 1, 1, 0), img2(100, 100, 1, 1, 0), img3(200, 200,
 			1, 1, 0);
 	cuadrado_centrado<double> (img1, 5.0, 5.0, img1.width() / 2.0,
-			img1.height() / 2.0, 0); //coef de matriz de respuesta al impulso pasa bajos de orden 21
-
+			img1.height() / 2.0, 0);
 	cuadrado_centrado<double> (img2, 5.0, 5.0, img2.width() / 2.0,
 			img2.height() / 2.0, 0);
 	cuadrado_centrado<double> (img3, 5.0, 5.0, img3.width() / 2.0,
 			img3.height() / 2.0, 0);
 
 	//muestro imagen original, filtro que se aplica centrado e imagen filtrada
-	//filtro 21x21:
+	//filtro 25x25:
 	CImgDisplay disp(img, "imagen original");
 	CImgDisplay disp1(get_magnitud<double> (img1, true),
-			"magnitud del filtro centrado - 21x21");
-	CImgDisplay disp2(img.get_convolve(img1),
-			"imagen filtrada con filtro centrado - 21x21 - convolucion");
+			"magnitud del filtro centrado - 25x25");
+	CImgDisplay disp2(img.get_FFT(true)[0].get_convolve(img1),
+			"imagen filtrada con filtro centrado - 25x25");
 
 	//filtro 100x100:
-	//obtengo version frecuencial del filtro de tam 100x100
-	img1.resize(100,100,1,1).display();
+
 	CImgDisplay disp3(get_magnitud<double> (img2, true),
 			"magnitud del filtro centrado - 100x100");
 	CImgDisplay disp4(img.get_FFT(true)[0].get_convolve(img2),
