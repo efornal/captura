bccb40ea0ee5bb9a9deb62a6579628c69798b4b2
esteban <efornal@gmail.com>
2010-05-18 01:38:42 -0300

    agregado visualizacion para espectro de la imagen filtrada

--- esteban/gtp06/e04/p01.cpp
+++ esteban/gtp06/e04/p01.cpp
@@ -31,14 +31,22 @@ int main( int argc, char **argv ) {
     disp.set_title("originial");
     disp2.set_title("modulo - modulo log - fase");
 
-    img.get_fft_modulo(false).normalize(0,255).display("localizar punto");
-
     filtro.get_normalize(0,255).display(disp3);
     disp3.set_title("filtro rechaza banda");
 
-    img.get_filtrada( filtro ).get_normalize(0,255).display(disp4);
+    CHImg<double> filtrada = img.get_filtrada( filtro );
+    filtrada.get_normalize(0,255).display(disp4);
+
     disp4.set_title("filtrada");
-        
+
+    CImgList<double> lista2( filtrada.get_fft_modulo().normalize(0,255),
+                             filtrada.get_fft_modulo_log().normalize(0,255),
+                             filtrada.get_fft_fase().normalize(0,255) );
+    lista2.display(disp5);
+    disp4.set_title("espectro de filtrada: modulo - modulo log - fase");
+
+    img.get_fft_modulo(false).normalize(0,255).display("acá localizar el punto");        
+
     while ( (!disp.is_closed() &&  !disp.is_keyQ()) ) {
         disp.wait();
     }
