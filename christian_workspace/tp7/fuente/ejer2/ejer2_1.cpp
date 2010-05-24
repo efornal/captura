42842e2ea362c218465f85b6568bc41b89c5178f
christian <c.pfarher@gmail.com>
2010-05-24 11:18:33 -0300

    emepzando ejer transf de hough

---
+++ christian_workspace/tp7/fuente/ejer2/ejer2_1.cpp
@@ -0,0 +1,47 @@
+/*
+ * ejer1_1y2.cpp
+ *
+ *  Created on: 22/05/2010
+ *      Author: christian
+ */
+
+#define cimg_use_fftw3 1
+
+#ifdef cimg_use_fftw3
+extern "C" {
+#include "fftw3.h"
+}
+#endif
+
+#include <iostream>
+#include <CImg.h>
+#include "../lib7/CPDSI_segmentacion.h"
+
+using namespace std;
+using namespace cimg_library;
+/*
+ * http://users.cs.cf.ac.uk/Paul.Rosin/CM0311/dual2/hough.html
+ * */
+int main(int argc, char **argv) {
+	CImg<double> img(500, 500, 1, 1, 0); //imagen original
+	img(0, 0) = 1;
+	img(0, 499) = 1;
+	img(249, 249) = 1;
+	img(0, 499) = 1;
+	img(499, 499) = 1;
+	img.normalize(0, 1);
+	CImg<double> Hough = hough_directa(img);
+	Hough.normalize(0, 1);
+	CImg<double> inversa = hough_inversa(img);
+	inversa.normalize(0, 1);
+	img.normalize(0, 1);
+
+	CImgDisplay disp(Hough, "Transf de Hough");
+	CImgDisplay disp1(inversa, "inversa de Hough");
+	CImgDisplay disp2(img, "imagen");
+
+	while (!disp.is_closed()) {
+		disp.wait();
+	}
+	return 0;
+}
