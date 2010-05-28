e4ca85595944a1b1212238f8f291005ba5e61344
christian <c.pfarher@gmail.com>
2010-05-28 14:21:40 -0300

    modifico unas indefiniciones aunque faltan algunas cosas mas

--- christian_workspace/tp7/fuente/ejer2/hough_animada.cpp
+++ christian_workspace/tp7/fuente/ejer2/hough_animada.cpp
@@ -25,26 +25,43 @@ using namespace cimg_library;
 int main(int argc, char **argv) {
 	CImg<double> img(500, 500, 1, 1, 0); //Creo imagen negro
 	img.normalize(0, 255);
+	//cout<<"use teclas X y Y para fijar el sentido de X y Y respectivamente"<<endl;
 	img(img.width() / 2, img.height() / 2, 0, 0) = 255.0; //punto blanco en el centro
 	CImgDisplay disp(img, "plano rho (y), tita (x -90 a 90)");
-	/*cimg::dialog("Animada Hough", "Realiza la transformada inversa de Hough a partir de la imagen que se muestra \n"
-			"se supone que la imagen que se muestra con el punto seria lo que dejaria luego de pasar un detector de \n"
-			"bordes y un thresold para que quede solo ese punto", "Aceptar");*/
 	CImg<double> inversa = hough_inversa(img); //transofrmada inversa del punto
 	CImgDisplay disp1(inversa, "transformada inversa de Hough de la imagen");
 	int posx, posy;
+	bool fija_x = false;
+	bool fija_y = false;
+	//todo: ojo hay unas indefinicinoes a arreglar no simpre funca bien esto...
 	while (!disp.is_closed()) {
 		disp.wait();
-		if (disp.is_event()){
-			posx=disp.mouse_x();
-			posy=disp.mouse_y();
+		if (disp.is_event()) {
+			if (disp.is_keyX()) {
+				fija_x = !fija_x;
+				cout << "Fija_x: " << fija_x << endl;
+			} else if (disp.is_keyY()) {
+				fija_y = !fija_y;
+				cout << "Fija_y: " << fija_y << endl;
+			}
+			if (!fija_x) { //si no esta fijada x
+				posx=disp.mouse_x();
+			}
+			if (!fija_y) { // si no esta fijada y
+				if (disp.mouse_y() == disp.width()-1) {
+					posy = disp.mouse_y() - 1.0;
+				} else if (disp.mouse_y() == 0) {
+					posy = 1.0;
+				} else {
+					posy = disp.mouse_y();
+				}
+			}
 			img.fill(0); //borro la imagen
-			img(posx, posy)=255; //dibujo el punto segun el mouse
+			img(posx, posy) = 255; //dibujo el punto segun el mouse
 
 			img.display(disp);
 			disp.set_title("plano rho (y), tita (x -90 a 90)");
-
-			inversa=hough_inversa(img);
+			inversa = hough_inversa(img);
 			inversa.display(disp1);
 			disp1.set_title("transformada inversa de Hough de la imagen");
 		}
