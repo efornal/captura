074bc22bf30d645656cf8f3d19caaa342ea6b171
christian <c.pfarher@gmail.com>
2010-05-29 12:11:52 -0300

    regeneracion binarios

---
+++ christian_workspace/tp7/fuente/ejer3/bassimio.cpp
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <iostream>
+#include "CImg.h"
+using namespace cimg_library;
+using namespace std;
+
+CImg<> img("../../imagenes/bone.tif"), img2(img.width(), img.height(), 1, 1, 0);
+
+int sX1 = 216, sY1 = 150;
+float tolerancia = 50.0;
+float intensidad = img(sX1, sY1);
+
+int maxX = img.width(), maxY = img.height();
+
+template <class T>
+CImg<T> segmenta(CImg<T> imagen_a_segmentar, CImg<T> &imagen_segmentada){
+	int img_width=imagen_a_segmentar.width();
+}
+void recursion(int x_inicial, int y_inicial) {
+	if (x > maxX || x < 0 || y > maxY || y < 0) {
+		return;
+	}
+	if ((img(x, y) <= (intensidad + tolerancia) && img(x, y) >= (intensidad
+			- tolerancia)) && img2(x, y) == 0) {
+		img2(x, y) = 1;
+		recursion(x + 1, y);
+		recursion(x, y + 1);
+		recursion(x - 1, y);
+		recursion(x - 1, y - 1);
+	} else
+		return;
+}
+
+int main(int argc, char** argv) {
+
+	img.channel(0);
+
+	recursion(sX1, sY1);
+
+	CImgDisplay vent1(img, "Original");
+	CImgDisplay vent2(img2, "A ver que hicimos");
+
+	while (!vent1.is_closed()) {
+	}
+	return (0);
+}
