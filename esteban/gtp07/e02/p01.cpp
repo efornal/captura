efa8f6b3c6e9a38611435257e85a56a8482fadd2
esteban <efornal@gmail.com>
2010-05-29 00:44:29 -0300

    patrones binarios de puntos blancos y su T Hough

--- esteban/gtp07/e02/p01.cpp
+++ esteban/gtp07/e02/p01.cpp
@@ -11,18 +11,46 @@ int main( int argc, char **argv ) {
     double dro   = cimg_option( "-dro", 1, "dro" );
     bool getHist = cimg_option( "-hist", 0, "getHist" );
 
-    CImgDisplay disp, disp2, disp3, disp4;
+    CImgDisplay disp, disp1, disp2, disp3, disp4, disp5, disp6;
     
-    CImg<double> img ( filename ), hough_dir, hough_inv;
-    img.channel(0);
+    CImg<double> img(400,400,1,1,0), 
+        hough_dir ,hough_fil,
+        hough_fil1, hough_fil2, hough_fil3, hough_fil4;
+
+    // puntos blanco de la imagen
+    img(0,0)     = 1;
+    img(399,0)   = 1;
+    img(0,399)   = 1;
+    img(399,399) = 1;
+    img(199,199) = 1;
+
     img.display(disp);
     
     hough_dir = hough_directa( img, dtita, dro, getHist); 
 
-    hough_inv = hough_inversa( img ); 
+    hough_fil  = hough_dir.get_threshold( hough_dir.max() );
+    hough_fil1 = hough_dir.get_threshold( hough_dir.max()-1 );
+    hough_fil2 = hough_dir.get_threshold( hough_dir.max()-2 );
 
-    hough_dir.normalize(0,255).display();
-    hough_inv.normalize(0,255).display();
+    hough_fil.display(disp1);
+    hough_inversa( hough_fil ).display( disp2 );
+
+    hough_fil1.display(disp3);
+    hough_inversa( hough_fil1 ).display( disp4 );
+
+    hough_fil2.display(disp4);
+    hough_inversa( hough_fil2 ).display( disp5 );
+
+    disp1.set_title("hough directa - umbral Max()");
+    disp2.set_title("hough inversa - punto Max()");
+
+    disp3.set_title("hough directa - umbral Max()-1");
+    disp4.set_title("hough inversa - punto Max()-1");
+
+    disp5.set_title("hough directa - umbral Max()-2");
+    disp6.set_title("hough inversa - punto Max()-2");
+
+    hough_dir.display("hough directa - 5 puntos");
 
     while ( (!disp.is_closed() &&  !disp.is_keyQ()) ) {
         disp.wait_all();
