7e36a8722d14618c1a4b7e00580daa8293c924f2
christian <c.pfarher@gmail.com>
2010-05-31 12:29:27 -0300

    empezando

---
+++ christian_workspace/tp8/fuente/ejer1/ejer1_1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : tp8.cpp
+// Author      : Christian Pfarher
+// Version     :
+// Copyright   : GNU Public License
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Captura y Procesamiento Digital de Imagenes 2010" << endl; // prints Captura y Procesamiento Digital de Imagenes 2010
+	return 0;
+}
