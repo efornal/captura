094a76bf6a1ad2401cfd0a4d53d79dd4daa8a397
christian <c.pfarher@gmail.com>
2010-06-02 14:20:27 -0300

    cambio comentarios

--- christian_workspace/tp4/fuente/ejer5/ejer5_1_b.cpp
+++ christian_workspace/tp4/fuente/ejer5/ejer5_1_b.cpp
@@ -21,11 +21,10 @@ int main(int argc, char **argv) {
 	CImg<float> imagen_oscura_R(dx, dy, 1, 3, 0), imagen_oscura_G(dx, dy, 1, 3,
 			0), imagen_oscura_B(dx, dy, 1, 3, 0);
 
-	descomponer_rgb <float> (imagen_oscura, imagen_oscura_R, imagen_oscura_G,
+	descomponer_rgb<float> (imagen_oscura, imagen_oscura_R, imagen_oscura_G,
 			imagen_oscura_B, false); //descompongo la imagen en 3 canales
 
-	CImgList<float> lista(imagen_oscura_R, imagen_oscura_G,
-			imagen_oscura_B);
+	CImgList<float> lista(imagen_oscura_R, imagen_oscura_G, imagen_oscura_B);
 	CImgDisplay disp1(lista, "canal R, canal G, canal B");
 	CImgDisplay dispaux1(imagen_oscura);
 	dispaux1.set_title("imagen oscura original");
@@ -42,11 +41,14 @@ int main(int argc, char **argv) {
 
 	// compongo la imagen para mostrarla:
 	/*CImgList<float> lista1(, imagen_oscura_R_eq,
-			imagen_oscura_G_eq, imagen_oscura_B_eq);*/ //esto lo coment porque en tendria que generar la imagen RGB ya que la funcion me la normaliza y me devuelve un solo canal... y si lo muestreo asi se va a ver _todo en rojo y va dar lugar a confusion
+	 imagen_oscura_G_eq, imagen_oscura_B_eq);*///esto lo coment porque en tendria que generar
+	//la imagen RGB ya que la funcion me la normaliza y me devuelve un solo canal... y si lo muestreo
+	//asi se va a ver _todo en rojo y va dar lugar a confusion
 
 	CImgDisplay dispaux2(componer_imagen<float> (imagen_oscura_R_eq,
 			imagen_oscura_G_eq, imagen_oscura_B_eq));
-	//FIXME: en la imagen ecualizada con los canales RGB por serparado quizas esta mas nitida que en la que ecualize sin separarlos..? la sombrilla, el edificio... pero es poca la dif.
+	//FIXME: en la imagen ecualizada con los canales RGB por serparado quizas esta mas nitida que en la que ecualize
+	// sin separarlos..? la sombrilla, el edificio... pero es poca la dif.
 
 	dispaux2.set_title("imagen ecualizada por canales RGB");
 	while (!disp1.is_closed()) {
