3567c476f8a2d9d6978b46b9c4f294bef6c7e7c3
mauro <maurotorrez@gmail.com>
2010-06-02 12:04:16 -0300

    comento.

--- mauro/tp4/ej6.cpp
+++ mauro/tp4/ej6.cpp
@@ -7,16 +7,30 @@
 using namespace cimg_library;
 using namespace std;
 
+/**
+ * max devuelve el máximo de los 3
+ */
 template<class T>
 T max( T a, T b, T c ) {
   return max( a, max( b, c ) );
 }
 
+/**
+ * max devuelve el mínimo de los 3
+ */
 template<class T>
 T min( T a, T b, T c ) {
   return min( a, min( b, c ) );
 }
 
+/**
+ * pertenece_entorno me dice si el color dado por r, g, b está dentro del entorno
+ * centrado en el punto (r0, g0, b0). Este entorno se define según el radio radio
+ * y la norma norma: si radio=4 y norma=2 será una esfera centrada en r0, g0, b0;
+ * si norma=1 la esfera será un "rombo", si norma=0 un cubo.
+ * /// NOTAR que definí la norma 0 queriendo hacer en realidad la norma infinito,
+ *     asi que si norma=0 calcula la norma infinito.
+ */
 template<class T>
 bool pertenece_entorno( T r, T g, T b, T r0, T g0, T b0, T radio=(T)2, int norma=2 ){
   switch( norma ) {
@@ -29,6 +43,15 @@ bool pertenece_entorno( T r, T g, T b, T r0, T g0, T b0, T radio=(T)2, int norma
   }
 }
 
+/**
+ * segmentar_rgb segmenta la imagen en el espacio de color, dejando solo aquellos
+ * colores que caben suficientemente "cerca" del entorno centrado en (r, g, b)
+ * con radio radio y calculado según la norma norma.
+ * @param img: imagen a segmentar
+ * @param r, g, b: coordenadas r g y b del centro del entorno
+ * @param radio: radio del entorno
+ * @param norma: norma usada para calcular el entorno en el espacio RGB
+ */
 template<class T>
 CImg<T> segmentar_rgb ( const CImg<T> &img, T r, T g, T b, T radio=(T)2, int norma=2 ) {
   CImg<bool> mascara( img.width(), img.height() );
@@ -46,6 +69,19 @@ CImg<T> segmentar_rgb ( const CImg<T> &img, T r, T g, T b, T radio=(T)2, int nor
   return resultado;
 }
 
+/**
+ * promedio_valores_rgb toma una vecindad centrada en (x,y) y con ancho
+ * y alto (2*entorno+1), de la imagen imagen y calcula los valores medios de
+ * los canales r, g y b, seteandolos en estos parametros pasados por referencia
+ * @param imagen: la imagen de la que se saca el entorno
+ * @param r, g, b: variables donde se guradará eel color r, g, b promedio
+ * @param entorno: el "radio" (norma infinito) de la subimagen que se usara para
+ *                 calcular el promedio
+ * @param x, y: coordenadas del punto que será centro de la subimagen
+ * /// NOTAR la diferencia entre este entorno y los de las funciones arriba, este
+ *     entorno es simplemente una medida espacial, arriba era un nombre genérico
+ *     de una figura en el espacio, determinada por la norma y el radio
+ */
 template<class T>
 void promedio_valores_rgb( CImg<T>imagen, T &r, T &g, T &b,
 			   int entorno, int x, int y ) {
