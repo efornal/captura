59ffa3cbe6b2e9edd93e10e234e14dd4f4eca06d
esteban <efornal@gmail.com>
2010-06-02 02:14:35 -0300

    intentos de restauracion por estimacion de funcion de degradacion

--- esteban/gtp06/e05/p01.cpp
+++ esteban/gtp06/e05/p01.cpp
@@ -1,5 +1,6 @@
 /**
  * Estimacion por observacion de la imagen
+ * FIXME: ver forma de hacerlo abajo, estan bien??
 */
 #include <CHImg.h>
 
@@ -20,12 +21,12 @@ int main( int argc, char **argv ) {
                                         "../../imagenes/del-libro/lib-sub-noise.tif",
                                          "ruta archivo imagen" );
 
-    CImgDisplay disp, disp2, disp3, disp4, disp5;
+    CImgDisplay disp, disp2, disp3, disp4, disp5, disp6;
     
     CHImg<double> original( orig ), ruidosa( noise ), 
         f ( sub_orig ), g( sub_noise ), 
-        F, G, H(f), restaurada, h;
-
+        F, G, H(f), restaurada1, restaurada2, h;
+    CImgList<double>  HH(original,original);
     f.channel(0);
     g.channel(0);
     CImgList<double> list1 ( f, g );
@@ -34,22 +35,35 @@ int main( int argc, char **argv ) {
     F = f.get_fft_modulo();
     G = g.get_fft_modulo();
 
+    //=============\
     // obtencion de funcion de degradacion:
+    cimg_forXY(F,x,y){
+        HH[0](x,y) = G(x,y)/F(x,y);
+        HH[1](x,y) = 0; //fase lineal
+    }
+    h = HH.get_FFT(true)[0];
+    restaurada1 = ruidosa.get_convolve(h); //OJO PODE ESTAR UN MES!!
+    //-------------/
+
+    //=============\
+    //2 forma restauracion: antes hay q hacer H del tamanio de F!
+    //Es factible de esta forma??
     cimg_forXY(F,x,y){
         H(x,y) = G(x,y)/F(x,y);
     }
-    
-    restaurada = original.get_filtrada( H ); // H es menor, mal!!
-    //sin embargo ya anda ...
+    restaurada2 = ruidosa.get_filtrada( H ); // H es menor, mal!!
+    //-------------/
 
     ruidosa.display(disp5);
-    restaurada.display(disp4);
+    restaurada1.display(disp4);
+    restaurada2.display(disp6);
     original.display(disp3);
 
     disp.set_title("sub-img original, sub-img ruidosa");
     disp3.set_title("imagen original sin ruido");
     disp5.set_title("imagen con ruido");
-    disp4.set_title("Imagen restaurada");
+    disp4.set_title("Imagen restaurada 1");
+    disp6.set_title("Imagen restaurada 2");
 
     while ( (!disp.is_closed() &&  !disp.is_keyQ()) ) {
         disp.wait();
