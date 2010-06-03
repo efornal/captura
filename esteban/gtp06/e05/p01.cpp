8737fefbf36717ddb3f30ce0b6a37a1e32fe7879
esteban <efornal@gmail.com>
2010-06-03 03:05:05 -0300

    correcciones de modulo

--- esteban/gtp06/e05/p01.cpp
+++ esteban/gtp06/e05/p01.cpp
@@ -25,33 +25,41 @@ int main( int argc, char **argv ) {
     
     CHImg<double> original( orig ), ruidosa( noise ), 
         f ( sub_orig ), g( sub_noise ), 
-        F, G, H(f), restaurada1, restaurada2, h;
-    CImgList<double>  HH(original,original);
+        restaurada1, restaurada2, h;
+    CImgList<double>  HH(original,original), F,G,H;
     f.channel(0);
     g.channel(0);
     CImgList<double> list1 ( f, g );
     list1.display(disp);
     
-    F = f.get_fft_modulo();
-    G = g.get_fft_modulo();
+    F = f.get_FFT();
+    G = g.get_FFT();
+
+    F = realimag2magfase(F);
+    G = realimag2magfase(G);
 
     //=============\
     // obtencion de funcion de degradacion:
-    cimg_forXY(F,x,y){
-        HH[0](x,y) = G(x,y)/F(x,y);
-        HH[1](x,y) = 0; //fase lineal
+    cimg_forXY(F[0],x,y){
+        HH[0](x,y) = G[0](x,y)/F[0](x,y);
+        HH[1](x,y) = G[0](x,y)/F[0](x,y); //fase lineal
     }
+
+    HH = magfase2realimag ( HH );
     h = HH.get_FFT(true)[0];
+    restaurada1=f;
     restaurada1 = ruidosa.get_convolve(h); //OJO PODE ESTAR UN MES!!
     //-------------/
 
     //=============\
     //2 forma restauracion: antes hay q hacer H del tamanio de F!
     //Es factible de esta forma??
-    cimg_forXY(F,x,y){
-        H(x,y) = G(x,y)/F(x,y);
+    cimg_forXY(F[0],x,y){
+        HH[0](x,y) = G[0](x,y)/F[0](x,y);
+        HH[1](x,y) = G[0](x,y)/F[0](x,y); //fase lineal
     }
-    restaurada2 = ruidosa.get_filtrada( H ); // H es menor, mal!!
+
+    restaurada2 = ruidosa.get_filtrada( H[0] ); // H es menor, mal!!
     //-------------/
 
     ruidosa.display(disp5);
